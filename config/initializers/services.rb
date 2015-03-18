6d10ad12293f2fd8506631e3795c76c42ce18cb7
Kushal Pisavadia <kushi.p@gmail.com>
2015-03-18 12:52:21 +0000

    Panopticon bearer token can be set through ENV var
    Allow overriding the Panopticon bearer token through the environment.

--- config/initializers/services.rb
+++ config/initializers/services.rb
@@ -24,4 +24,7 @@ require 'gds_api/publishing_api'
 CollectionsPublisher.services(:publishing_api, GdsApi::PublishingApi.new(Plek.new.find('publishing-api')))
 
 require 'gds_api/panopticon'
-CollectionsPublisher.services(:panopticon, GdsApi::Panopticon.new(Plek.new.find('panopticon'), bearer_token: 'example'))
+CollectionsPublisher.services(
+  :panopticon,
+  GdsApi::Panopticon.new(Plek.new.find('panopticon'),
+                         bearer_token: ENV['PANOPTICON_BEARER_TOKEN'] || 'example'))
