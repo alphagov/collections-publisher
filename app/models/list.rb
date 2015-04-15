447840d63ff0f7d8eda153b145afc4d1ad29d14b
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-04-15 12:32:28 +0000

    Annotate all models.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: lists
+#
+#  id        :integer          not null, primary key
+#  name      :string(255)
+#  sector_id :string(255)
+#  index     :integer          default(0), not null
+#  dirty     :boolean          default(TRUE), not null
+#
+# Indexes
+#
+#  index_lists_on_sector_id  (sector_id)
+#
+
 class List < ActiveRecord::Base
   has_many :contents, dependent: :destroy
 
