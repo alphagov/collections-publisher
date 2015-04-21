7549e58cb839f8b29ac45c208b95ae4a1e34f357
Kushal Pisavadia <kushi.p@gmail.com>
2015-04-21 16:14:45 +0100

    Allow associating topics with mainstream browse
    The only thing to be aware of is that the controller `update` method
    here tries to map over the submitted `Array` of ids (which are `String`
    values) and tries to find the respective `Topic` objects.
    
    This is because of how the association between `Topic` and
    `MainstreamBrowsePage` models work using the `TagAssociation`. Rails
    isn't clever enough with it's magic to deal with this cleanly.

--- app/controllers/mainstream_browse_pages_controller.rb
+++ app/controllers/mainstream_browse_pages_controller.rb
@@ -4,7 +4,35 @@ class MainstreamBrowsePagesController < ApplicationController
 
   before_filter :require_gds_editor_permissions!
 
+  def edit
+    @topics = Topic.all
+  end
+
   def show; end
+
+  def update
+    # Convert the String ids to Topic objects so that
+    # `update_attributes` correct updates and associates
+    # them with the given `MainstreamBrowsePage` object.
+    if params.require(:mainstream_browse_page).key? :topics
+      topic_ids = params.require(:mainstream_browse_page)[:topics]
+      topics = topic_ids.reject(&:blank?).map { |t| Topic.find(t) }
+      msb_params = tag_params.merge({"topics" => topics})
+    else
+      msb_params = tag_params
+    end
+
+    if @resource.update_attributes(msb_params)
+      PanopticonNotifier.update_tag(
+        presenter_klass.new(@resource)
+      )
+
+      redirect_to mainstream_browse_page_path(@resource)
+    else
+      @topics = Topic.all
+      render :edit
+    end
+  end
 
 private
   def presenter_klass
--- spec/features/associating_topics_to_mainstream_browse_pages_spec.rb
+++ spec/features/associating_topics_to_mainstream_browse_pages_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe "associating topics to mainstream browse pages" do
   before do
     stub_user.permissions << "GDS Editor"
+    stub_all_panopticon_tag_calls
   end
 
   context "existing mainstream browse pages" do
@@ -20,6 +21,19 @@ describe "associating topics to mainstream browse pages" do
 
       visit mainstream_browse_page_path(mainstream_browse_page)
 
+      expect(page.status_code).to eq(200)
+      expect(page).to have_content(topic.title)
+    end
+
+    it "should allow associating topics" do
+      visit edit_mainstream_browse_page_path(mainstream_browse_page)
+
+      within "form.resource" do
+        select topic.title, :from => "mainstream_browse_page_topics"
+        click_on "Save"
+      end
+
+      mainstream_browse_page.reload
       expect(page.status_code).to eq(200)
       expect(page).to have_content(topic.title)
     end
