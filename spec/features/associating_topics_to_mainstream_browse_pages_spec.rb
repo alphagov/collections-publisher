756b1145ffea44335245bebe0b105f166991c001
Kushal Pisavadia <kushi.p@gmail.com>
2015-04-22 11:06:01 +0100

    Refactor tests to take into account multiple topics
    The form input field is a multi-select so it's cleaner to make all of
    our tests use multiple topics as they will naturally work for the
    singular case.

--- spec/features/associating_topics_to_mainstream_browse_pages_spec.rb
+++ spec/features/associating_topics_to_mainstream_browse_pages_spec.rb
@@ -12,45 +12,17 @@ describe "associating topics to mainstream browse pages" do
     let!(:topic_two)              { create(:topic) }
 
     it "should show any topics that are associated" do
-      mainstream_browse_page.topics << topic
+      mainstream_browse_page.topics = [topic, topic_two]
       expect(mainstream_browse_page.save).to be_true
 
       visit mainstream_browse_page_path(mainstream_browse_page)
 
       expect(page.status_code).to eq(200)
       expect(page).to have_content(topic.title)
+      expect(page).to have_content(topic_two.title)
     end
 
-    it "should allow associating a topic" do
-      visit edit_mainstream_browse_page_path(mainstream_browse_page)
-
-      within "form.resource" do
-        select topic.title, :from => "mainstream_browse_page_topics"
-        click_on "Save"
-      end
-
-      expect(page.status_code).to eq(200)
-      expect(page).to have_content(topic.title)
-    end
-
-    it "should allow removing associated topics" do
-      mainstream_browse_page.topics << topic
-      expect(mainstream_browse_page.save).to be_true
-
-      visit edit_mainstream_browse_page_path(mainstream_browse_page)
-
-      within "form.resource" do
-        unselect topic.title, :from => "mainstream_browse_page_topics"
-        click_on "Save"
-      end
-
-      visit mainstream_browse_page_path(mainstream_browse_page)
-
-      expect(page.status_code).to eq(200)
-      expect(page).to_not have_content(topic.title)
-    end
-
-    it "should allow associating multiple topics" do
+    it "should allow associating topics" do
       visit edit_mainstream_browse_page_path(mainstream_browse_page)
 
       within "form.resource" do
@@ -62,6 +34,25 @@ describe "associating topics to mainstream browse pages" do
       expect(page.status_code).to eq(200)
       expect(page).to have_content(topic.title)
       expect(page).to have_content(topic_two.title)
+    end
+
+    it "should allow removing associated topics" do
+      mainstream_browse_page.topics = [topic, topic_two]
+      expect(mainstream_browse_page.save).to be_true
+
+      visit edit_mainstream_browse_page_path(mainstream_browse_page)
+
+      within "form.resource" do
+        unselect topic.title, :from => "mainstream_browse_page_topics"
+        unselect topic_two.title, :from => "mainstream_browse_page_topics"
+        click_on "Save"
+      end
+
+      visit mainstream_browse_page_path(mainstream_browse_page)
+
+      expect(page.status_code).to eq(200)
+      expect(page).to_not have_content(topic.title)
+      expect(page).to_not have_content(topic_two.title)
     end
   end
 end
