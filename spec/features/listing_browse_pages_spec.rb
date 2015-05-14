67dfa6f41088d60a24cf75b7662f6f0759ca374d
tijmen <tijmen@gmail.com>
2015-05-14 11:15:31 +0100

    Add more acceptance comments for feature tests

--- spec/features/listing_browse_pages_spec.rb
+++ spec/features/listing_browse_pages_spec.rb
@@ -6,11 +6,14 @@ RSpec.describe 'listing browse pages' do
   end
 
   it 'only shows parent browse pages' do
+    # Given there are browse pages with children
     parent_browse_pages = create_list(:mainstream_browse_page, 5)
     child_browse_pages = create_list(:mainstream_browse_page, 5, parent: parent_browse_pages.first )
 
+    # When I visit the browse page
     visit mainstream_browse_pages_path
 
+    # Then I should see a list of children
     within '.tags-list' do
       parent_browse_pages.each do |parent_browse_page|
         expect(page).to have_link(parent_browse_page.title,
@@ -28,14 +31,18 @@ RSpec.describe 'listing browse pages' do
     let(:child_browse_page) { create(:mainstream_browse_page, parent: parent_browse_page) }
 
     it 'shows the link given a top-level parent' do
+      # When I visit a parent browse page
       visit mainstream_browse_page_path(parent_browse_page)
 
+      # Then I should be able to add a child page
       expect(page).to have_link('Add child page')
     end
 
     it 'does not show the link given a child page' do
+      # When I visit a child browse page
       visit mainstream_browse_page_path(child_browse_page)
 
+      # Then I should not be able to add a child page
       expect(page).to_not have_link('Add child page')
     end
   end
