532367cda47d619789872bdbf1c42957c2016e28
tijmen <tijmen@gmail.com>
2015-06-10 10:04:42 +0100

    Replace js-confirm by Rails native
    data-confirm will be picked up by Rails UJS and does the same thing as
    our `js-confirm` button.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,16 +12,6 @@ GOVUK.curatedLists.init();
   window.GOVUK = window.GOVUK || {};
   var $ = window.jQuery;
 
-  $('.js-confirm').each(function(_, button) {
-    var $button = $(button);
-
-    $button.closest('form').submit(function(event) {
-      if (!confirm($button.data('confirm-text'))) {
-        event.preventDefault();
-      }
-    });
-  });
-
   $(document).ready(function() {
     $(".select2").select2({ allowClear: true });
   });
