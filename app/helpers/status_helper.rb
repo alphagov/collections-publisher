464897ceddfbc4e55457b3afac63c63ad3cff64a
tijmen <tijmen@gmail.com>
2015-06-11 13:41:03 +0100

    Add dirty state for tags
    When a list is updated the tag is marked as "dirty", which means it has
    changes that have not been published to the content store and aren't
    shown on the site.
    
    Adding a label will help users to see which tags have unpublished
    changes - currently only the appearance of the "Publish" button would
    indicate the published state.

--- app/helpers/status_helper.rb
+++ app/helpers/status_helper.rb
@@ -8,8 +8,20 @@ module StatusHelper
     content_tag :span, text, class: "label label-#{class_name}"
   end
 
+  def labels_for_tag(tag)
+    labels = []
+    labels << draft_tag if tag.draft?
+    labels << beta_tag if tag.beta?
+    labels << dirty_tag if tag.dirty?
+    labels.join(' ')
+  end
+
   def beta_tag
     status 'In Beta', :warning
+  end
+
+  def dirty_tag
+    status 'Unpublished changes', :danger
   end
 
   def draft_tag
