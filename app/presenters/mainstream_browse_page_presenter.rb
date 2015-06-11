ada9579cd4fe70d4e09036d5fc0bf25d0fab7135
Jacopo Scotti <jacopojackscotti@gmail.com>
2015-06-11 09:50:12 +0000

    Send top and second-level pages to content-store
    Added the following parameters to the links method in
    the Mainstream Browse Presenter:
    
    - top_level_browse_page
    - top_level_browse_pages
    - second_level_browse_pages
    
    When collections-publisher sends a mainstream browse
    page document to the content store, it should include
    the full details needed to render the first two
    columns of the page, so that the collections frontend
    doesn't need to make a call to content-api to render
    these columns of a mainstream browse page.

--- app/presenters/mainstream_browse_page_presenter.rb
+++ app/presenters/mainstream_browse_page_presenter.rb
@@ -9,6 +9,28 @@ private
   def links
     super.merge(
       "related_topics" => @tag.topics.order(:title).map(&:content_id),
+      "top_level_browse_page" => top_level_browse_page_id,
+      "top_level_browse_pages" => @tag.class.sorted_parents.map(&:content_id),
+      "second_level_browse_pages" => second_level_browse_pages,
     )
   end
+
+private
+
+  def top_level_browse_page_id
+    if @tag.has_parent?
+      [@tag.parent.content_id]
+    else
+      []
+    end
+  end
+
+  def second_level_browse_pages
+    if @tag.child?
+      @tag.parent.sorted_children.map(&:content_id)
+    else
+      @tag.sorted_children.map(&:content_id)
+    end
+  end
+
 end
