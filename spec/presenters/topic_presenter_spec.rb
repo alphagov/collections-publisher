e010224eb7b54710bfca06254099947385e6d548
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-06-17 12:54:59 +0000

    Refactor TopicPresenter tests.
    Create separate contexts for a top-level topic, and a subtopic.  This
    makes it much clearer what's expected for each case.

--- spec/presenters/topic_presenter_spec.rb
+++ spec/presenters/topic_presenter_spec.rb
@@ -2,55 +2,101 @@ require 'rails_helper'
 
 RSpec.describe TopicPresenter do
   describe "rendering for publishing-api" do
-    let(:parent) { create(:topic, :slug => 'oil-and-gas') }
-    let(:topic) {
-      create(:topic, {
-        :parent => parent,
-        :slug => 'offshore',
-        :title => 'Offshore',
-        :description => 'Oil rigs, pipelines etc.',
-      })
-    }
-    let(:presenter) { TopicPresenter.new(topic) }
-    let(:presented_data) { presenter.render_for_publishing_api }
+    context "for a top-level topic" do
+      let(:topic) {
+        create(:topic, {
+          :slug => 'working-at-sea',
+          :title => 'Working at sea',
+          :description => 'The sea, the sky, the sea, the sky...',
+        })
+      }
+      let(:presenter) { TopicPresenter.new(topic) }
+      let(:presented_data) { presenter.render_for_publishing_api }
 
-    it "returns the base_path for the browse page" do
-      expect(presenter.base_path).to eq("/oil-and-gas/offshore")
-    end
-
-    it "includes the base fields" do
-      expect(presented_data).to include({
-        :content_id => topic.content_id,
-        :format => 'topic',
-        :title => 'Offshore',
-        :description => 'Oil rigs, pipelines etc.',
-        :locale => 'en',
-        :need_ids => [],
-        :publishing_app => 'collections-publisher',
-        :rendering_app => 'collections',
-        :redirects => [],
-        :update_type => "major",
-      })
-    end
-
-    it "is valid against the schema", :schema_test => true do
-      expect(presented_data).to be_valid_against_schema('topic')
-    end
-
-    it "is valid against the schema for a top-level topic", :schema_test => true do
-      data = TopicPresenter.new(parent).render_for_publishing_api
-      expect(data).to be_valid_against_schema('topic')
-    end
-
-    it "sets public_updated_at based on the browse page update time" do
-      Timecop.travel 3.hours.ago do
-        topic.save!
+      it "includes the base fields" do
+        expect(presented_data).to include({
+          :content_id => topic.content_id,
+          :format => 'topic',
+          :title => 'Working at sea',
+          :description => 'The sea, the sky, the sea, the sky...',
+          :locale => 'en',
+          :need_ids => [],
+          :publishing_app => 'collections-publisher',
+          :rendering_app => 'collections',
+          :redirects => [],
+          :update_type => "major",
+        })
       end
 
-      expect(presented_data[:public_updated_at]).to eq(topic.updated_at.iso8601)
+      it "is valid against the schema", :schema_test => true do
+        expect(presented_data).to be_valid_against_schema('topic')
+      end
+
+      it "returns the base_path for the topic" do
+        expect(presenter.base_path).to eq("/working-at-sea")
+      end
+
+      it "sets public_updated_at based on the topic update time" do
+        Timecop.travel 3.hours.ago do
+          topic.save!
+        end
+        expect(presented_data[:public_updated_at]).to eq(topic.updated_at.iso8601)
+      end
+
+      it "includes the base route" do
+        expect(presented_data[:routes]).to eq([
+          {:path => "/working-at-sea", :type => "exact"},
+        ])
+      end
+
+      it "has no links" do
+        expect(presented_data[:links]).to eq({})
+      end
     end
 
-    describe "routing" do
+    context "for a subtopic" do
+      let(:parent) { create(:topic, :slug => 'oil-and-gas') }
+      let(:topic) {
+        create(:topic, {
+          :parent => parent,
+          :slug => 'offshore',
+          :title => 'Offshore',
+          :description => 'Oil rigs, pipelines etc.',
+        })
+      }
+      let(:presenter) { TopicPresenter.new(topic) }
+      let(:presented_data) { presenter.render_for_publishing_api }
+
+      it "returns the base_path for the subtopic" do
+        expect(presenter.base_path).to eq("/oil-and-gas/offshore")
+      end
+
+      it "includes the base fields" do
+        expect(presented_data).to include({
+          :content_id => topic.content_id,
+          :format => 'topic',
+          :title => 'Offshore',
+          :description => 'Oil rigs, pipelines etc.',
+          :locale => 'en',
+          :need_ids => [],
+          :publishing_app => 'collections-publisher',
+          :rendering_app => 'collections',
+          :redirects => [],
+          :update_type => "major",
+        })
+      end
+
+      it "is valid against the schema", :schema_test => true do
+        expect(presented_data).to be_valid_against_schema('topic')
+      end
+
+      it "sets public_updated_at based on the topic update time" do
+        Timecop.travel 3.hours.ago do
+          topic.save!
+        end
+        expect(presented_data[:public_updated_at]).to eq(topic.updated_at.iso8601)
+      end
+
       it "includes routes for latest, and email_signups in addition to base route" do
         expect(presented_data[:routes]).to eq([
           {:path => "/oil-and-gas/offshore", :type => "exact"},
@@ -60,17 +106,9 @@ RSpec.describe TopicPresenter do
         ])
       end
 
-      it "only includes the base route for a top-level topic" do
-        data = TopicPresenter.new(parent).render_for_publishing_api
-
-        expect(data[:routes]).to eq([
-          {:path => "/oil-and-gas", :type => "exact"},
-        ])
+      it "has no links" do
+        expect(presented_data[:links]).to eq({})
       end
-    end
-
-    it "has no links" do
-      expect(presented_data[:links]).to eq({})
     end
   end
 end
