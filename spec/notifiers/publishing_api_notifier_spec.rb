e2a74501647d1b86cf92d128f6ba709b739fba69
Stuart Gale <bishboria@gmail.com>
2015-06-22 12:31:11 +0100

    Add tests for sending batch changes

--- spec/notifiers/publishing_api_notifier_spec.rb
+++ spec/notifiers/publishing_api_notifier_spec.rb
@@ -2,7 +2,54 @@ require "rails_helper"
 
 RSpec.describe PublishingAPINotifier do
   include ContentStoreHelpers
-  before { stub_content_store! }
+
+  let(:publishing_api) { instance_double("GdsApi::PublishingApi", :put_content_item => nil, :put_draft_content_item => nil) }
+
+  before do
+    stub_content_store!
+    allow(CollectionsPublisher).to receive(:services).with(:publishing_api).and_return(publishing_api)
+  end
+
+  describe "batch_send_to_publishing_api" do
+    #   a     d   - top level
+    #  /
+    # b           - second level
+    before do
+      @a = browse_page("a")
+      @d = browse_page("d")
+      @b = browse_page("b", @a)
+    end
+
+    it "sends siblings and the parent when a sibling is added" do
+      c = browse_page("c", @a)
+
+      [c, @a, @b].each do |page|
+        expect(PublishingAPINotifier).to receive(:send_to_publishing_api).with(page)
+      end
+
+      expect(PublishingAPINotifier).to_not receive(:send_to_publishing_api).with(@d)
+
+      PublishingAPINotifier.batch_send_to_publishing_api(c)
+    end
+
+    it "sends the full hierarchy when a parent is added" do
+      e = browse_page("e")
+
+      [@b, @a, @d, e].each do |page|
+        expect(PublishingAPINotifier).to receive(:send_to_publishing_api).with(page)
+      end
+
+      PublishingAPINotifier.batch_send_to_publishing_api(e)
+    end
+
+    it "queues work correctly" do
+      Sidekiq::Testing.fake! do
+        expect {
+          PublishingAPINotifier.batch_send_to_publishing_api(@a)
+        }.to change(PublishingAPINotifier::QueueWorker.jobs, :size).by(3)
+      end
+    end
+  end
 
   describe "#send_to_publishing_api" do
     context "for a draft tag" do
