9e7d9d3f263c12bc81c2d996b0c8bfb4eb8145e8
tijmen <tijmen@gmail.com>
2015-06-23 13:55:03 +0100

    Rename publish_lists method
    The `publish_lists` method more clearly communicates the intent (the
    publishing of lists, not the tag).

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   before_filter :find_tag
   before_filter :require_gds_editor_permissions_to_edit_browse_pages!
 
-  def republish
+  def publish_lists
     PublishingAPINotifier.send_to_publishing_api(@tag)
     redirect_to :back
   end
