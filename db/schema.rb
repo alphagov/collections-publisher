1867e73b9ac2837987f59ce7d5bcec9291ff35e8
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-06-25 15:19:21 +0000

    Add redirects for 2 top-level browse pages.
    These redirects are currently hardcoded in the collections app[1].
    Adding them here will allow us to remove all route registration code
    from the collections app. It will also create redirect content items
    for these ensuring that the base_paths are reserved by this app and
    can't be overwritten.
    
    [1] https://github.com/alphagov/collections/blob/1f55dcc/lib/tasks/router.rake

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625150427) do
+ActiveRecord::Schema.define(version: 20150625151822) do
 
   create_table "list_items", force: :cascade do |t|
     t.string   "api_url",    limit: 255
