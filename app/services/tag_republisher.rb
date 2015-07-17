e4a2a5acdb9396b32d6108938aaaaa2b8a84fd1b
tijmen <tijmen@gmail.com>
2015-07-17 13:15:19 +0100

    Also retry publishing of root browse page
    Currently, when the `/browse` page publish fails because of a timeout,
    it is not retried. This fixes that.

--- app/services/tag_republisher.rb
+++ app/services/tag_republisher.rb
@@ -16,10 +16,12 @@ class TagRepublisher
     log "Done: #{done} tags sent to publishing-api."
 
     log "Sending root browse page to publishing-api"
-    publishing_api.put_content_item(
-      "/browse",
-      RootBrowsePagePresenter.new.render_for_publishing_api
-    )
+    with_retry do
+      publishing_api.put_content_item(
+        "/browse",
+        RootBrowsePagePresenter.new.render_for_publishing_api
+      )
+    end
 
     log "All done"
   end
@@ -27,14 +29,20 @@ class TagRepublisher
 private
 
   def republish_tag(tag)
+    with_retry do
+      PublishingAPINotifier.new(tag).send_single_tag_to_publishing_api
+    end
+  end
+
+  def with_retry
     retries = 0
 
     begin
-      PublishingAPINotifier.new(tag).send_single_tag_to_publishing_api
+      yield
     rescue GdsApi::TimedOutException, Timeout::Error => e
       retries += 1
       if retries <= 3
-        log "Timeout (tag #{tag.base_path}): retry #{retries}"
+        log "Timeout: retry #{retries}"
         sleep 0.5
         retry
       end
