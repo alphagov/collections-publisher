37a5657e8f5785bbeeaf163f0cebe4371d29a746
tijmen <tijmen@gmail.com>
2015-09-07 15:18:58 +0100

    Extract a TagPublisher class
    This will make it easier to make modifications to the logic.

---
+++ app/services/tag_publisher.rb
@@ -0,0 +1,14 @@
+class TagPublisher
+  attr_reader :tag
+
+  def initialize(tag)
+    @tag = tag
+  end
+
+  def publish
+    tag.publish!
+    PanopticonNotifier.publish_tag(TagPresenter.presenter_for(tag))
+    PublishingAPINotifier.send_to_publishing_api(tag)
+    RummagerNotifier.new(tag).notify
+  end
+end
