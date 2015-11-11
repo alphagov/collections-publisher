ff49429980d1b3dcbe3e9e5d695ebadd60582d39
Mo Baig <mo@mobaig.com>
2015-11-11 15:43:57 +0000

    Add missing timestamp updates to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110152255) do
+ActiveRecord::Schema.define(version: 20151110220915) do
 
   create_table "list_items", force: :cascade do |t|
     t.string   "base_path",  limit: 255
