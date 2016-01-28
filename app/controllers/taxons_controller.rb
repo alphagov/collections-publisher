e2a8e57150e98d1c1c9708caab39c85dc6efd4b8
Mo Baig <mo@mobaig.com>
2016-01-28 12:50:18 +0000

    Update TaxonForm to gracefully handle taxons with blank links
    - Check if a links>parent value is available before attempting to assign
      the attribute in the form object.
    - Move all logic around instantiating the taxon form object into a
      factory method on TaxonForm.

--- app/controllers/taxons_controller.rb
+++ app/controllers/taxons_controller.rb
@@ -18,16 +18,7 @@ class TaxonsController < ApplicationController
 
   def edit
     @taxons_for_select = Taxonomy::TaxonFetcher.new.taxons_for_select
-
-    content_item = Services.publishing_api.get_content(params[:id])
-    links = Services.publishing_api.get_links(params[:id]).links
-
-    @taxon = TaxonForm.new(
-      content_id: content_item.content_id,
-      title: content_item.title,
-      base_path: content_item.base_path,
-      parent: links.parent.to_a.first,
-    )
+    @taxon = TaxonForm.build(content_id: params[:id])
   end
 
   def update
