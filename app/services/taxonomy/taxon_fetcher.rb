3109de5acff382e9fad038931ad6ce4a6ecfa8bf
Carlos Vilhena <carlosvilhena@gmail.com>
2016-07-13 16:32:24 +0100

    Add parents_for_taxon_form method to taxon fetcher
    Why?
    We need a way to show parents for a given taxon. As the API returns a list of
    taxon names and content ids, we can re-use that to select the correct parents
    based on their content_id.

--- app/services/taxonomy/taxon_fetcher.rb
+++ app/services/taxonomy/taxon_fetcher.rb
@@ -2,13 +2,20 @@ module Taxonomy
   # Return a list of taxons from the publishing API with links included.
   class TaxonFetcher
     def taxons
-      Services.publishing_api.get_linkables(
-        document_type: 'taxon'
-      ).sort_by { |taxon| taxon["title"] }
+      @taxons ||=
+        Services.publishing_api.get_linkables(
+          document_type: 'taxon'
+        ).sort_by { |taxon| taxon["title"] }
     end
 
     def taxons_for_select
       taxons.map { |taxon| [taxon['title'], taxon['content_id']] }
+    end
+
+    def parents_for_taxon_form(taxon_form)
+      taxons.select do |taxon|
+        taxon_form.parents.include?(taxon['content_id'])
+      end
     end
   end
 end
--- spec/services/taxonomy/taxon_fetcher_spec.rb
+++ spec/services/taxonomy/taxon_fetcher_spec.rb
@@ -3,6 +3,7 @@ require 'gds_api/test_helpers/publishing_api_v2'
 
 RSpec.describe Taxonomy::TaxonFetcher do
   include GdsApi::TestHelpers::PublishingApiV2
+
   describe '#taxons' do
     it 'retrieves content items from publishing api and orders by title' do
       linkables = [
@@ -17,6 +18,33 @@ RSpec.describe Taxonomy::TaxonFetcher do
 
       expect(result.first["title"]).to eq("aha")
       expect(result.last["title"]).to eq("foo")
+    end
+  end
+
+  describe '#parents_for_taxon_form' do
+    let(:taxon_id_1) { SecureRandom.uuid }
+    let(:taxon_id_2) { SecureRandom.uuid }
+    let(:taxon_form) do
+      double('TaxonForm', parents: [taxon_id_1, taxon_id_2])
+    end
+    let(:link_1) do
+      {"title" => "foo", "base_path" => "/foo", "content_id" => taxon_id_1 }
+    end
+    let(:link_2) do
+      {"title" => "bar", "base_path" => "/bar", "content_id" => taxon_id_2}
+    end
+    let(:link_3) do
+      {"title" => "aha", "base_path" => "/aha", "content_id" => SecureRandom.uuid}
+    end
+    let(:linkables) { [link_1, link_2, link_3] }
+
+    it 'returns the parent taxons for a given taxon' do
+      publishing_api_has_linkables(linkables, document_type: 'taxon')
+      result = described_class.new.parents_for_taxon_form(taxon_form)
+
+      expect(result.count).to eq(2)
+      expect(result).to include(link_1)
+      expect(result).to include(link_2)
     end
   end
 end
