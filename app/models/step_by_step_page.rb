9969e14c2ce23285413e2ff4e77a02e3ccd752d0
David Silva <david.silva@digital.cabinet-office.gov.uk>
2018-03-06 16:15:59 +0000

    Add SlugValidator
    This prevents users from creating a duplicate slug, to do so we call
    Publishing API to know if there's already a document with the same slug.

--- app/models/step_by_step_page.rb
+++ app/models/step_by_step_page.rb
@@ -2,9 +2,7 @@ class StepByStepPage < ApplicationRecord
   has_many :steps, -> { order(position: :asc) }, dependent: :destroy
 
   validates :title, :slug, :introduction, :description, presence: true
-  validates :slug, format: { with: /\A([a-z0-9]+-)*[a-z0-9]+\z/ }
-  validates :slug, uniqueness: true
-
+  validates :slug, format: { with: /\A([a-z0-9]+-)*[a-z0-9]+\z/ }, uniqueness: true, slug: true
   before_validation :generate_content_id, on: :create
 
 private
