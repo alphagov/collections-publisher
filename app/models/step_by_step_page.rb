890352b47ce31ece2ebe73878c39eb2ecd7b3321
David Silva <david.silva@digital.cabinet-office.gov.uk>
2018-03-15 11:42:44 +0000

    Don't change the slug once the Step Page is saved
    When the step by step page is saved and the editor goes back to edit it
    it should not be able to edit the slug, neither the slug should be validated again
    as the slug already exists.

--- app/models/step_by_step_page.rb
+++ app/models/step_by_step_page.rb
@@ -2,7 +2,8 @@ class StepByStepPage < ApplicationRecord
   has_many :steps, -> { order(position: :asc) }, dependent: :destroy
 
   validates :title, :slug, :introduction, :description, presence: true
-  validates :slug, format: { with: /\A([a-z0-9]+-)*[a-z0-9]+\z/ }, uniqueness: true, slug: true
+  validates :slug, format: { with: /\A([a-z0-9]+-)*[a-z0-9]+\z/ }, uniqueness: true
+  validates :slug, slug: true, on: :create
   before_validation :generate_content_id, on: :create
 
 private
