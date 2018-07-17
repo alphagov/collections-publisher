49cf4d2a8549a0f767fe63365d9b1e0ea1b759e0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-07-17 15:43:17 +0100

    Add access limiting token to the querystring when requested
    Adding an access limiting token to the querystring allows uses who don’t have a signon account to view
    draft content.
    
    At the moment this only applies to step by step navigation.
    The token is decoded by [authenticating proxy](https://github.com/alphagov/authenticating-proxy) which then allows
    the user to bypass authentication.

--- app/helpers/jwt_helper.rb
+++ app/helpers/jwt_helper.rb
@@ -13,4 +13,19 @@ module JwtHelper
       "%08x-%04x-%04x-%04x-%04x%08x" % ary
     end
   end
+
+  def access_limited_preview_url(slug, auth_bypass_id)
+    token = jwt_token(auth_bypass_id)
+    "#{Services.draft_origin}/#{slug.sub(/^\//, '')}?token=#{token}"
+  end
+
+private
+
+  def jwt_token(auth_bypass_id)
+    JWT.encode({ 'sub' => auth_bypass_id }, jwt_auth_secret, 'HS256')
+  end
+
+  def jwt_auth_secret
+    ENV['JWT_AUTH_SECRET']
+  end
 end
--- spec/helpers/jwt_helper_spec.rb
+++ spec/helpers/jwt_helper_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe JwtHelper do
       expect(auth_bypass_token(123)).to eq("61363635-6134-4539-b230-343232663964")
     end
   end
+
+  describe "#access_limited_preview_url" do
+    it "appends a valid JWT token in the querystring" do
+      slug = "step-nav-slug"
+
+      expected_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEyM30.xTbOwaYOmzgd16a-cMrkBIeB5lY7qneU66rqFTwTKPw"
+      expected_url = "#{Services.draft_origin}/#{slug}?token=#{expected_token}"
+
+      expect(access_limited_preview_url(slug, 123)).to eq(expected_url)
+    end
+  end
 end
