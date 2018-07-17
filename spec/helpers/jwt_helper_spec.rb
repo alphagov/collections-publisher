1035255bfc2e9f863d55ad7d614803df09085718
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-07-17 16:59:42 +0100

    Fix up to First commit

--- spec/helpers/jwt_helper_spec.rb
+++ spec/helpers/jwt_helper_spec.rb
@@ -1,12 +1,6 @@
 require "rails_helper"
 
 RSpec.describe JwtHelper do
-  describe "#auth_bypass_token" do
-    it 'should create a predictable hex string based on the id' do
-      expect(auth_bypass_token(123)).to eq("61363635-6134-4539-b230-343232663964")
-    end
-  end
-
   describe "#access_limited_preview_url" do
     it "appends a valid JWT token in the querystring" do
       slug = "step-nav-slug"
