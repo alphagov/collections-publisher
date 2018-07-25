8a4759d7af2b64fc5551ac2617b219eed875cfb5
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-07-25 10:24:47 +0000

    Removes link_check controller
    This commit removes the link_check controller and moves the functions to the Step model

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,4 +2,16 @@ class Step < ApplicationRecord
   belongs_to :step_by_step_page
   validates_presence_of :step_by_step_page
   validates :title, :logic, presence: true
+
+  def batch_link_report
+    @base_api_url = Plek.find("link-checker-api")
+    @api = GdsApi::LinkCheckerApi.new(@base_api_url)
+    @api.get_batch(find_batch_id)
+    # params
+  end
+
+    # gets the most recent batch id for the step by step the user is on
+  def batch_link_report_id
+      
+  end
 end
