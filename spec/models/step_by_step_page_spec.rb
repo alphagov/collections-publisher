eaf58d69c1f0bc57e00641186194b7e3f40fc13b
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2018-07-26 12:54:20 +0000

    Add filter option for step nav statuses

--- spec/models/step_by_step_page_spec.rb
+++ spec/models/step_by_step_page_spec.rb
@@ -144,6 +144,7 @@ RSpec.describe StepByStepPage do
 
         expect(step_by_step_page.draft_updated_at).to be_within(1.second).of nowish
         expect(step_by_step_page.has_draft?).to be true
+        expect(step_by_step_page.status[:name]).to eq('draft')
       end
     end
 
@@ -163,6 +164,7 @@ RSpec.describe StepByStepPage do
         expect(step_by_step_page.published_at).to eq(step_by_step_page.draft_updated_at)
         expect(step_by_step_page.has_been_published?).to be true
         expect(step_by_step_page.has_draft?).to be false
+        expect(step_by_step_page.status[:name]).to eq('live')
       end
     end
 
