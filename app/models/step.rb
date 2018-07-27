2b8682f27fe485ba6c65acd942e19100279d5ebc
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-07-27 10:20:06 +0000

    Adds tests for new Step model functionality
    This commit extends the Step model to include batch report data

--- app/models/step.rb
+++ app/models/step.rb
@@ -4,12 +4,25 @@ class Step < ApplicationRecord
   validates :title, :logic, presence: true
   has_many :link_check_report
 
-  def batch_link_report
-    Services.link_checker_api.get_batch(find_batch_id)
+  # returns true if there are broken links
+  def broken_links?
+    batch_link_report.totals.broken > 0
   end
 
-    # gets the most recent batch id for a step
+  # returns a BatchReport object from the GDS API helper for the link-checker service
+  def batch_link_report
+    @batch_link_report ||= Services.link_checker_api.get_batch(batch_link_report_id)
+  end
+
+  # gets the most recent batch id for a step
   def batch_link_report_id
-    LinkCheckReport.where(step_id: self.id).last.id
+    LinkCheckReport.where(step_id: self.id).last.batch_id
+  end
+
+  # returns an array of LinkReport objects if there are any with a broken status or an empty array if not
+  def broken_links
+    batch_link_report.links.map do |link|
+      link if link.status == 'broken'
+    end
   end
 end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Step do
   before do
     allow(Services.publishing_api).to receive(:lookup_content_id)
+    create(:link_check_report, batch_id: 1, step_id: step_item.id)
+    create(:link_check_report, completed: Time.now, batch_id: 2, step_id: step_item.id)
   end
 
   let(:step_item) { build(:step) }
@@ -41,9 +43,12 @@ RSpec.describe Step do
 
   describe 'link reports' do
     it 'should return the most recent batch_id' do
-      create(:link_check_report, batch_id: 1, step_id: step_item.id)
-      create(:link_check_report, completed: Time.now, batch_id: 2, step_id: step_item.id)
       expect(step_item.batch_link_report_id).to eql 2
+    end
+
+    it 'should get a mock back of a batch link report that has the id of 2' do
+      batch_link_report = step_item.batch_link_report
+      expect(batch_link_report.id).to eql 2
     end
   end
 end
