29de4bed38cee69722b4017c5f8c606aebdf5cf8
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-07-30 10:50:32 +0100

    Cleans code for linter

--- app/models/step.rb
+++ app/models/step.rb
@@ -4,20 +4,18 @@ class Step < ApplicationRecord
   validates :title, :logic, presence: true
   has_many :link_check_report
 
-  # returns true if there are broken links
   def broken_links?
-    most_recent_link_check_report.present? && number_of_broken_links > 0
+    most_recent_link_check_report.present? && number_of_broken_links.positive?
   end
 
-  # returns an array of LinkReport objects if there are any with a broken status or an empty array if not
   def broken_links
     batch_link_report.links.keep_if do |link|
-      link.fetch('status') == 'broken' 
+      link.fetch('status') == 'broken'
     end
   end
 
 private
-  # returns a BatchReport object from the GDS API helper for the link-checker service
+
   def batch_link_report
     @batch_link_report ||= Services.link_checker_api.get_batch(batch_link_report_id)
   end
