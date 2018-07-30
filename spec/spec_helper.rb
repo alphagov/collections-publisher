285a46bce44b4e42c26a6714b37cf94bc975f6a6
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-07-30 11:15:07 +0100

    Still trying to please the linter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -142,6 +142,7 @@ RSpec.configure do |config|
              'Accept-Encoding' => 'gzip, deflate',
              'Host' => 'link-checker-api.test.gov.uk',
              'User-Agent' => 'gds-api-adapters/52.6.0 ()'
-             }).to_return(status: 200, body: expected_response, headers: {})
+            }
+            ).to_return(status: 200, body: expected_response, headers: {})
   end
 end
