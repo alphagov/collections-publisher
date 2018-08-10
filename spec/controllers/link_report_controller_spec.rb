678c6a83ce3b136b32db135df3e8f26e6ffc4e47
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-08-10 14:54:15 +0100

    Adds webhook to receive batch
    This commit adds a webhook that receives a batch from the link-checker-api and updates that batch's completed time. This enables us to search for the most recent batch_id and retrieve that from the link-checker-api. We've implemented a webhook to avoid spamming the link-checker-api.

---
+++ spec/controllers/link_report_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require 'gds_api/test_helpers/link_checker_api'
+
+RSpec.describe LinkReportController do
+  include GdsApi::TestHelpers::LinkCheckerApi
+  describe '.update' do
+    it 'should update an existing LinkReport' do
+      time = Time.now
+      create(:link_report, batch_id: 1234, completed: nil)
+      patch :update, params: { id: 1234, completed_at: time }
+      expect(LinkReport.find_by(batch_id: 1234).completed).to be_within(1.second).of time
+    end
+    context "when the batch_id doesn't exist" do
+      it 'should fail gracefully' do
+        time = Time.now
+        expect { patch :update, params: { id: 1234, completed_at: time } }.to_not raise_error
+      end
+    end
+  end
+end
