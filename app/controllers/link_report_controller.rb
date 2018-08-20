3cd45514d780141f91c7ff9e32b6dfdeafdf9dbe
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-08-20 14:19:35 +0000

    Fixes webhook issues
    This commit fixes some issues with the webhook not acting as it ought to. I have changed the route from patch to post and improved the tests so that they more closely model the data sent back from the api. I've also added `skip_before_action :verify_authenticity_token`. This was causing an error because the controller expected a CSRF token, which we don't use here because it's an API endpoint.

--- app/controllers/link_report_controller.rb
+++ app/controllers/link_report_controller.rb
@@ -1,18 +1,15 @@
 class LinkReportController < ApplicationController
   before_action :set_link_report
+  skip_before_action :verify_authenticity_token
 
   def update
     unless set_link_report.nil?
-      @link_report.completed = link_report_params[:completed_at]
+      @link_report.completed = params[:completed_at]
       @link_report.save
     end
   end
 
 private
-
-  def link_report_params
-    params.permit(:id, :completed_at)
-  end
 
   def set_link_report
     @link_report = LinkReport.find_by(batch_id: params[:id])
--- spec/controllers/link_report_controller_spec.rb
+++ spec/controllers/link_report_controller_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe LinkReportController do
     it 'should update an existing LinkReport' do
       time = Time.now
       create(:link_report, batch_id: 1234, completed: nil)
-      patch :update, params: { id: 1234, completed_at: time }
+      post :update, params: link_checker_api_batch_report_hash(id: 1234, completed_at: time)
       expect(LinkReport.find_by(batch_id: 1234).completed).to be_within(1.second).of time
     end
     context "when the batch_id doesn't exist" do
       it 'should fail gracefully' do
         time = Time.now
-        expect { patch :update, params: { id: 1234, completed_at: time } }.to_not raise_error
+        expect { post :update, params: link_checker_api_batch_report_hash(id: 1234, completed_at: time) }.to_not raise_error
       end
     end
   end
