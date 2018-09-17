b4c57453b81f4a5372011952c7ee2f9f9370de8c
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-09-17 18:46:16 +0100

    Store the latest edition number in change notes
    Sometimes content designers need record change notes for step by steps that have already been published.
    
    Getting the edition number and storing the edition number for these change notes allows us to keep the version
    history of the step by step up to date.
    
    The default behaviour of not storing the edition number of the step by step in a change note, for step by steps in a draft
    state is not being changed.

--- app/controllers/internal_change_notes_controller.rb
+++ app/controllers/internal_change_notes_controller.rb
@@ -15,10 +15,28 @@ private
   end
 
   def other_fields
-    {
+    fields = {
       step_by_step_page_id: step_by_step_page.id,
       created_at: Time.now,
       author: current_user.name
     }
+
+    fields[:edition_number] = latest_edition_number if save_edition_number?
+
+    fields
+  end
+
+  def save_edition_number?
+    step_by_step_page.has_been_published? && !step_by_step_page.has_draft?
+  end
+
+  # state_history returns a hash like {"3"=>"draft", "2"=>"published", "1"=>"superseded"}
+  # so we need to get the highest value for a key.
+  def latest_edition_number
+    @latest_edition_number ||= content_item[:state_history].keys.max
+  end
+
+  def content_item
+    Services.publishing_api.get_content(step_by_step_page.content_id)
   end
 end
