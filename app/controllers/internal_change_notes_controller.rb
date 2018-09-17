346499a7d8c698f9f7253b5509277726526e6cf8
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2018-09-17 18:11:44 +0100

    Refactor how an InternalChangeNote is stored
    This refactor will make it easier to optionally add a value for edition_number to the `other_fields` in the next commit.

--- app/controllers/internal_change_notes_controller.rb
+++ app/controllers/internal_change_notes_controller.rb
@@ -1,6 +1,6 @@
 class InternalChangeNotesController < ApplicationController
   def create
-    InternalChangeNote.create(internal_change_note_params.merge(step_by_step_page_id: step_by_step_page.id, created_at: Time.now, author: current_user.name))
+    InternalChangeNote.create(required_fields.merge(other_fields))
     redirect_to step_by_step_page_internal_change_notes_path, notice: 'Change note was successfully added.'
   end
 
@@ -10,7 +10,15 @@ private
     StepByStepPage.find(params[:step_by_step_page_id])
   end
 
-  def internal_change_note_params
+  def required_fields
     params.require(:internal_change_note).permit(:description)
+  end
+
+  def other_fields
+    {
+      step_by_step_page_id: step_by_step_page.id,
+      created_at: Time.now,
+      author: current_user.name
+    }
   end
 end
