088e78d058f68750ed9ceea5969bb8d30e76bc02
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-12-21 15:10:51 +0000

    Update expected JSON Web Token values for the jwt upgrade

--- spec/lib/jwt_helper_spec.rb
+++ spec/lib/jwt_helper_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe JwtHelper do
     it "appends a valid JWT token in the querystring" do
       url = "http://step-nav-slug"
 
-      expected_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEyM30.xTbOwaYOmzgd16a-cMrkBIeB5lY7qneU66rqFTwTKPw"
+      expected_token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEyM30.-r0B06VSBvNo2Z_WMSuC9G65epKc3ZdYxM8L-3xfsvI"
       expected_url = "#{url}?token=#{expected_token}"
 
       expect(described_class.access_limited_preview_url(url, 123)).to eq(expected_url)
