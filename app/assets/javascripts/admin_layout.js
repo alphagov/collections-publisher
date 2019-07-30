086e122b5c824213c66a2f42cfcaab3d95902813
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-07-30 10:34:29 +0100

    JS updates and test fix

---
+++ app/assets/javascripts/admin_layout.js
@@ -0,0 +1,7 @@
+//= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/all_components
+//= require jquery-ui.sortable.min
+//= require ./confirm
+//= require ./steps
+
+GOVUK.stepByStepPublisher.init();
