bcd99c22e52a3d5b0027865b58959419300bf2d5
Alex Jurubita <alexandru.jurubita@digital.cabinet-office.gov.uk>
2019-08-12 13:00:53 +0100

    Add markdown-editor component
    Copy a stripped version of markdown-editor (https://content-publisher.integration.publishing.service.gov.uk/component-guide/markdown_editor) from content-publisher

---
+++ spec/components/markdown_editor_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Markdown editor", type: :view do
+  it "fails to render when no data is given" do
+    assert_raises do
+      render "components/markdown_editor"
+    end
+  end
+
+  it "renders a textarea with a label and toolbar" do
+    render "components/markdown_editor",  label: {
+                                            text: "Body"
+                                          },
+                                          textarea: {
+                                            name: "markdown-editor",
+                                            id: "markdown-editor"
+                                          }
+
+    assert_select ".govuk-label[for='markdown-editor']", text: 'Body'
+    assert_select ".app-c-markdown-editor__toolbar-group[for='markdown-editor']"
+    assert_select ".govuk-textarea[id='markdown-editor']"
+  end
+end
