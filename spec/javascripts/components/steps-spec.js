1a09886084564f447d357cb010451a819b098742
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-08-12 16:19:51 +0100

    Test that rows matching search term are displayed
    - Added id 'hooks' to fixture data for easy assertions
    - Adds search input to fixture data

--- spec/javascripts/components/steps-spec.js
+++ spec/javascripts/components/steps-spec.js
@@ -5,12 +5,14 @@ describe('Step by step publisher component', function () {
   'use strict'
 
   var container
+  var stepByStepRows
 
   beforeEach(function () {
     container = document.createElement('div')
     container.innerHTML = loadFixtures('overview-table.html')
 
     document.body.appendChild(container)
+    stepByStepRows = $('.step-by-step-list__table .govuk-table__row')
     GOVUK.stepByStepPublisher.init()
   })
 
@@ -20,15 +22,34 @@ describe('Step by step publisher component', function () {
 
   describe('#bindOverviewTableFilter', function () {
     it('should display all step by steps by default', function () {
-      var stepByStepRows = $('.step-by-step-list__table .govuk-table__row')
-      expectAllRowsToBeVisible(stepByStepRows)
+      expectAllRowsToBeVisible()
+    })
+
+    it('should display results where the title matches the search term', function () {
+      searchFor('fish')
+      expectOnlyVisibleRowToBe('js-test__row-id--fish')
     })
   })
 
-  function expectAllRowsToBeVisible (stepByStepRows) {
+  function searchFor (searchTerm) {
+    $('#filterTableInput').val(searchTerm).trigger('keyup')
+  }
+
+  function expectAllRowsToBeVisible () {
     expect(stepByStepRows.length).toEqual(3)
     stepByStepRows.each(function () {
       expect($(this)).toBeVisible()
+    })
+  }
+
+  function expectOnlyVisibleRowToBe (rowId) {
+    stepByStepRows.each(function () {
+      var row = $(this)
+      if (row.attr('id') === rowId) {
+        expect(row).toBeVisible()
+      } else {
+        expect(row).toBeHidden()
+      }
     })
   }
 })
