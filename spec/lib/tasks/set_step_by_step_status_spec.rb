59d88f574b9645c8daaa4510d867d43a50b94f7c
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-09-06 13:00:00 +0100

    Add rake task to set status of step-by-steps
    "update_column" bypasses the model validations so it is being used in
    the tests to simulate the state of existing step-by-steps that won't
    have anything set for the status.
    
    It's not possible to use the existing scheduled_for_publishing?` method
    from the model because it has already been updated to include a check
    for `status == "scheduled"`.

---
+++ spec/lib/tasks/set_step_by_step_status_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe "rake step_by_step:set_status", type: :task do
+  before do
+    allow(Services.publishing_api).to receive(:lookup_content_id)
+
+    Rails.application.load_tasks
+    Rake::Task['step_by_step:set_status'].reenable
+  end
+
+  it "sets the status to draft" do
+    step_by_step = create(:draft_step_by_step_page)
+    step_by_step.update_column(:status, "")
+
+    Rake::Task['step_by_step:set_status'].invoke
+
+    expect(step_by_step.reload.status).to be_draft
+  end
+
+  it "sets the status to published" do
+    step_by_step = create(:published_step_by_step_page)
+    step_by_step.update_column(:status, "")
+
+    Rake::Task['step_by_step:set_status'].invoke
+
+    expect(step_by_step.reload.status).to be_published
+  end
+
+  it "sets the status to scheduled" do
+    step_by_step = create(:scheduled_step_by_step_page)
+    step_by_step.update_column(:status, "")
+
+    Rake::Task['step_by_step:set_status'].invoke
+
+    expect(step_by_step.reload.status).to be_scheduled
+  end
+
+  it "sets the status to draft when there are unpublished changes" do
+    step_by_step = create(:published_step_by_step_page, draft_updated_at: Time.zone.now)
+    step_by_step.update_column(:status, "")
+
+    Rake::Task['step_by_step:set_status'].invoke
+
+    expect(step_by_step.reload.status).to be_draft
+  end
+
+  it "sets the status to draft if step by step has been unpublished" do
+    step_by_step = create(:published_step_by_step_page)
+    step_by_step.update_columns(
+      status: "",
+      published_at: nil,
+      draft_updated_at: nil
+    )
+
+    Rake::Task['step_by_step:set_status'].invoke
+
+    expect(step_by_step.reload.status).to be_draft
+  end
+end
