482c6db871262035e5ccd13b0c9ad19d3280d66f
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-09-10 11:25:06 +0100

    Refactor the status prerequisite validator

--- app/validators/status_prerequisite_validator.rb
+++ app/validators/status_prerequisite_validator.rb
@@ -1,15 +1,25 @@
 class StatusPrerequisiteValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value == "scheduled"
-      return true if record.has_draft? && record.scheduled_at.present?
+      return true if can_be_scheduled?(record)
 
       record.errors[attribute] << "#{value}, requires a draft and scheduled_at date to be present"
     end
 
     if value == "submitted_for_2i"
-      return true if record.has_draft? && record.review_requester.present?
+      return true if can_be_submitted_for_2i?(record)
 
       record.errors[attribute] << "#{value}, requires a draft and review_requester to be present"
     end
+  end
+
+private
+
+  def can_be_scheduled?(record)
+    record.has_draft? && record.scheduled_at.present?
+  end
+
+  def can_be_submitted_for_2i?(record)
+    record.has_draft? && record.review_requester.present?
   end
 end
