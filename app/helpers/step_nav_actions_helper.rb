9c7114ed7a468304863099b2552aeae51b6b8f2b
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-09-27 09:36:19 +0100

    Allow an in_review step by step to be taken over by another reviewer

--- app/helpers/step_nav_actions_helper.rb
+++ app/helpers/step_nav_actions_helper.rb
@@ -1,6 +1,19 @@
 module StepNavActionsHelper
   def can_review?(step_by_step_page, user)
+    can_claim_first_review?(step_by_step_page, user) ||
+      can_take_over_review?(step_by_step_page, user)
+  end
+
+private
+
+  def can_claim_first_review?(step_by_step_page, user)
     step_by_step_page.status.submitted_for_2i? &&
       step_by_step_page.review_requester_id != user.uid
+  end
+
+  def can_take_over_review?(step_by_step_page, user)
+    step_by_step_page.status.in_review? &&
+      step_by_step_page.review_requester_id != user.uid &&
+      step_by_step_page.reviewer_id != user.uid
   end
 end
--- spec/helpers/step_nav_actions_helper_spec.rb
+++ spec/helpers/step_nav_actions_helper_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe StepNavActionsHelper do
   let(:step_by_step_page) { create(:draft_step_by_step_page) }
   let(:user) { create(:user) }
+  let(:reviewer_user) { create(:user) }
+  let(:second_reviewer_user) { create(:user) }
 
   before do
     allow(Services.publishing_api).to receive(:lookup_content_id)
@@ -17,6 +19,26 @@ RSpec.describe StepNavActionsHelper do
       step_by_step_page.status = "submitted_for_2i"
       step_by_step_page.review_requester_id = user.uid
       expect(helper.can_review?(step_by_step_page, user)).to be false
+    end
+
+    it "returns true if another user tries to claim the review" do
+      step_by_step_page.status = "submitted_for_2i"
+      step_by_step_page.review_requester_id = user.uid
+      expect(helper.can_review?(step_by_step_page, reviewer_user)).to be true
+    end
+
+    it "returns true if another reviewer tries to take over the review" do
+      step_by_step_page.status = "in_review"
+      step_by_step_page.review_requester_id = user.uid
+      step_by_step_page.reviewer_id = reviewer_user.uid
+      expect(helper.can_review?(step_by_step_page, second_reviewer_user)).to be true
+    end
+
+    it "returns false if the reviewer tries to claim review again" do
+      step_by_step_page.status = "in_review"
+      step_by_step_page.review_requester_id = user.uid
+      step_by_step_page.reviewer_id = reviewer_user.uid
+      expect(helper.can_review?(step_by_step_page, reviewer_user)).to be false
     end
   end
 end
