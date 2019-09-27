7bd55ab2486dfbc8005c0a7cc5553b8278a5568c
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-09-27 09:19:40 +0100

    Migrate existing steps to ensure they each have a position
    I've added code which fixes the position of new step by step steps,
    but we need to deal with the possibility of an existing step by step
    that has not yet been reordered. In this case, no steps would have a
    'position' and the 'make_step_positions_sequential' would not be able
    to sort by 'position'.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_124652) do
+ActiveRecord::Schema.define(version: 2019_09_27_080905) do
 
   create_table "internal_change_notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "author"
