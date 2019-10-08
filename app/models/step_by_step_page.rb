827b0e622f912826c401aac692f0189018af16cf
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-10-08 12:36:03 +0100

    Create auth_bypass_id values without using Content ID
    Removes the dependence on the Content ID for generating auth_bypass_id
    values.  Instead we will use a completely random value.
    
    Note that we continue to use a UUID to ensure compliance with the
    relevant content schema.

--- app/models/step_by_step_page.rb
+++ app/models/step_by_step_page.rb
@@ -30,6 +30,7 @@ class StepByStepPage < ApplicationRecord
   scope :by_title, -> { order(:title) }
 
   attribute :content_id, :string, default: -> { SecureRandom.uuid }
+  attribute :auth_bypass_id, :string, default: -> { SecureRandom.uuid }
 
   def has_been_published?
     published_at.present?
@@ -127,21 +128,6 @@ class StepByStepPage < ApplicationRecord
 
   def steps_have_content?
     steps.any? && steps.map(&:contents).all?(&:present?)
-  end
-
-  # Create a deterministic, but unique token that will be used to give one-time
-  # access to a piece of draft content.
-  # This token is created by using an id that should be unique so that there is
-  # little chance of the same token being created to view another piece of content.
-  # The code to create the token has been "borrowed" from SecureRandom.uuid,
-  # See: http://ruby-doc.org/stdlib-1.9.3/libdoc/securerandom/rdoc/SecureRandom.html#uuid-method
-  def auth_bypass_id
-    @auth_bypass_id ||= begin
-      ary = Digest::SHA256.hexdigest(content_id.to_s).unpack("NnnnnN")
-      ary[2] = (ary[2] & 0x0fff) | 0x4000
-      ary[3] = (ary[3] & 0x3fff) | 0x8000
-      "%08x-%04x-%04x-%04x-%04x%08x" % ary
-    end
   end
 
   def links_last_checked_date
