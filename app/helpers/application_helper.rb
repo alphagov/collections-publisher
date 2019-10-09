bf3d0465b95dc77f82c01c038fdf0d40214f2f61
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2019-10-09 10:43:27 +0100

    Split 'preview_url' into authenticated vs anonymous methods
    Rather than changing the behaviour by passing an extra parameter, it
    is more explicit to use well named methods instead. This also frees
    us up to make logic changes to only the 'step_by_step_preview_url' method,
    leaving the 'draft_govuk_url' method untouched.
    
    We've decided to simplify the new 'draft_govuk_url' method to take
    a path rather than a slug, so that it can be used more generically.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,15 @@ module ApplicationHelper
     Plek.new.external_url_for("content-tagger")
   end
 
-  def preview_url(slug, auth_bypass_id: nil)
-    url = "#{Plek.new.external_url_for('draft-origin')}/#{slug.sub(/^\//, '')}"
+  def draft_govuk_url(path)
+    Plek.find("draft-origin", external: true) + path
+  end
 
-    if auth_bypass_id.present?
-      url = JwtHelper.access_limited_preview_url(url, auth_bypass_id)
-    end
-
-    url
+  def step_by_step_preview_url(step_by_step_page)
+    JwtHelper.access_limited_preview_url(
+      draft_govuk_url("/#{step_by_step_page.slug}"),
+      step_by_step_page.auth_bypass_id,
+    )
   end
 
   def published_url(slug)
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,26 +1,30 @@
 require "rails_helper"
 
 RSpec.describe ApplicationHelper do
-  describe "#preview_url" do
+  describe "preview URLs" do
     before do
       allow(Services.publishing_api).to receive(:lookup_content_id)
     end
 
     let(:step_nav) { create(:step_by_step_page) }
 
-    it "returns a link to the draft content" do
-      expected_url = "#{draft_origin_url}/#{step_nav.slug}"
+    describe "#draft_govuk_url" do
+      it "returns a link to the draft content" do
+        expected_url = "#{draft_origin_url}/#{step_nav.slug}"
 
-      expect(helper.preview_url(step_nav.slug)).to eq(expected_url)
+        expect(helper.draft_govuk_url("/#{step_nav.slug}")).to eq(expected_url)
+      end
     end
 
-    it "appends a valid JWT token in the querystring" do
-      allow(step_nav).to receive(:auth_bypass_id) { "123" }
+    describe "#step_by_step_preview_url" do
+      it "appends a valid JWT token in the querystring" do
+        allow(step_nav).to receive(:auth_bypass_id) { "123" }
 
-      expected_token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjMifQ.f7yK2FqG2LVLauPM3K5UYkFPlabvft-lsGeGUhgQwNs"
-      expected_url = "#{expected_step_nav_preview_url}?token=#{expected_token}"
+        expected_token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjMifQ.f7yK2FqG2LVLauPM3K5UYkFPlabvft-lsGeGUhgQwNs"
+        expected_url = "#{expected_step_nav_preview_url}?token=#{expected_token}"
 
-      expect(helper.preview_url(step_nav.slug, auth_bypass_id: step_nav.auth_bypass_id)).to eq(expected_url)
+        expect(helper.step_by_step_preview_url(step_nav)).to eq(expected_url)
+      end
     end
   end
 
