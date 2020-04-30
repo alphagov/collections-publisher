c38a38bf94502602e8abc47c00fe5cfdc4ec9fcd
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-04-30 10:04:26 +0100

    Disable Rails/UniqueValidationWithoutIndex
    This disables a Cop that would require a migration to fix. While
    this Cop is valid for future code, it would require excessive effort
    to fix the issue for existing code.

--- app/models/secondary_content_link.rb
+++ app/models/secondary_content_link.rb
@@ -3,7 +3,7 @@ class SecondaryContentLink < ApplicationRecord
   validates :step_by_step_page, presence: true
 
   validates :title, :base_path, :content_id, :publishing_app, :schema_name, :step_by_step_page_id, presence: true
-  validates :base_path, uniqueness: { scope: :step_by_step_page_id, case_sensitive: false }
+  validates :base_path, uniqueness: { scope: :step_by_step_page_id, case_sensitive: false } # rubocop:disable Rails/UniqueValidationWithoutIndex
 
   def smartanswer?
     schema_name == "transaction" && publishing_app == "smartanswers"
