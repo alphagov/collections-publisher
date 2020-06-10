b01b747db1cd3c0846dad3c33d88ffbc11cbaefa
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-06-10 11:36:56 +0100

    Create CoronavirusPage model
    - Currently, the coronavirus model will only be associated with
    the sections/accordions. But as we move more of the content out of the
    the yaml file on govuk-coronavirus-content and into the database, it
    will grow.
    - It also is only representing the landing page at this stage.
    But we can add a type column at a later date so it represents
    all the coronavirus hub pages, which will be linked to from the
    coronaviruspages controller index page.

---
+++ app/models/coronavirus_page.rb
@@ -0,0 +1,3 @@
+class CoronavirusPage < ApplicationRecord
+  has_many :sub_sections
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_150108) do
+ActiveRecord::Schema.define(version: 2020_06_09_181902) do
+
+  create_table "coronavirus_pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "sections_title"
+    t.string "base_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "internal_change_notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "author"
