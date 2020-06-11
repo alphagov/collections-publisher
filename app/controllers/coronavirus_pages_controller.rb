892e2c6b3a346fff74324647dedfc003ee1064bf
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-06-11 19:21:52 +0100

    Move pages configuration out of controller

--- app/controllers/coronavirus_pages_controller.rb
+++ app/controllers/coronavirus_pages_controller.rb
@@ -4,10 +4,10 @@ class CoronavirusPagesController < ApplicationController
   layout "admin_layout"
 
   def index
-    links = all_pages_configuration.keys.map do |page|
+    links = page_configs.keys.map do |page|
       {
         slug: page.to_s,
-        title: all_pages_configuration[page][:name],
+        title: page_configs[page][:name],
       }
     end
 
@@ -119,7 +119,7 @@ private
   end
 
   def page_config
-    all_pages_configuration[page_type]
+    page_configs[page_type]
   end
 
   def slug
@@ -154,40 +154,7 @@ private
     ]
   end
 
-  def all_pages_configuration
-    {
-      landing:
-        {
-          name: "Coronavirus landing page",
-          content_id: "774cee22-d896-44c1-a611-e3109cce8eae".freeze,
-          raw_content_url: "https://raw.githubusercontent.com/alphagov/govuk-coronavirus-content/master/content/coronavirus_landing_page.yml".freeze,
-          base_path: "/coronavirus",
-          github_url: "https://github.com/alphagov/govuk-coronavirus-content/blob/master/content/coronavirus_landing_page.yml",
-        },
-      business:
-        {
-          name: "Business support page",
-          content_id: "09944b84-02ba-4742-a696-9e562fc9b29d".freeze,
-          raw_content_url: "https://raw.githubusercontent.com/alphagov/govuk-coronavirus-content/master/content/coronavirus_business_page.yml".freeze,
-          base_path: "/coronavirus/business-support",
-          github_url: "https://github.com/alphagov/govuk-coronavirus-content/blob/master/content/coronavirus_business_page.yml",
-        },
-      education:
-        {
-          name: "Education page",
-          content_id: "b350e61d-1db9-4cc2-bb44-fab02882ac25".freeze,
-          raw_content_url: "https://raw.githubusercontent.com/alphagov/govuk-coronavirus-content/master/content/coronavirus_education_page.yml".freeze,
-          base_path: "/coronavirus/education-and-childcare",
-          github_url: "https://github.com/alphagov/govuk-coronavirus-content/blob/master/content/coronavirus_education_page.yml",
-        },
-      employees:
-        {
-          name: "Worker page",
-          content_id: "5ebf285a-9165-476c-be90-66b9729f50da".freeze,
-          raw_content_url: "https://raw.githubusercontent.com/alphagov/govuk-coronavirus-content/master/content/coronavirus_worker_page.yml".freeze,
-          base_path: "/coronavirus/worker-support",
-          github_url: "https://github.com/alphagov/govuk-coronavirus-content/blob/master/content/coronavirus_worker_page.yml",
-        },
-    }
+  def page_configs
+    CoronavirusPages::Configuration.all_pages
   end
 end
