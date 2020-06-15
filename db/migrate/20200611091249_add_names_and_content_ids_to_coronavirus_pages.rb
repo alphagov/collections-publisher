a1ab262c3cf27e56b7f20474a13bcbad091b988f
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-06-15 10:05:03 +0100

    Silence rubcop errors on two migrations that have already been run.

--- db/migrate/20200611091249_add_names_and_content_ids_to_coronavirus_pages.rb
+++ db/migrate/20200611091249_add_names_and_content_ids_to_coronavirus_pages.rb
@@ -1,7 +1,9 @@
 class AddNamesAndContentIdsToCoronavirusPages < ActiveRecord::Migration[6.0]
+  # rubocop:disable Rails/BulkChangeTable
   def change
     add_column :coronavirus_pages, :name, :string
     add_column :coronavirus_pages, :slug, :string
     add_column :coronavirus_pages, :content_id, :string
   end
+  # rubocop:enable Rails/BulkChangeTable
 end
