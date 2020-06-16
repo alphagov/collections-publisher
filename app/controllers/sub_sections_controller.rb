8ac40fe9a08ee49bedcdaf777bf86ed0c5318648
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-06-16 16:59:24 +0100

    Wire up controller actions with templates - coronavirus show page - sub_section edit page

--- app/controllers/sub_sections_controller.rb
+++ app/controllers/sub_sections_controller.rb
@@ -2,7 +2,10 @@ class SubSectionsController < ApplicationController
   before_action :require_coronavirus_editor_permissions!
   layout "admin_layout"
 
-  def edit; end
+  def edit
+    @coronavirus_page = CoronavirusPage.find_by(slug: params[:coronavirus_page_slug])
+    @sub_section = @coronavirus_page.sub_sections.find(params[:id])
+  end
 
   def update; end
 end
