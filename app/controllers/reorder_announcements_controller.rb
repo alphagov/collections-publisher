c8c9e2d6d3394628a22b54a83201a1a45a791804
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-11-03 17:22:49 +0000

    Save announcements after reordering
    This adds functionality to the "save" button added in the previous
    commit.

--- app/controllers/reorder_announcements_controller.rb
+++ app/controllers/reorder_announcements_controller.rb
@@ -6,7 +6,20 @@ class ReorderAnnouncementsController < ApplicationController
     coronavirus_page
   end
 
-  def update; end
+  def update
+    reordered_announcements = JSON.parse(params[:announcement_order_save])
+    reordered_announcements.each do |announcement_data|
+      announcement = coronavirus_page.announcements.find(announcement_data["id"])
+      announcement.update!(position: announcement_data["position"])
+    end
+
+    if draft_updater.send
+      redirect_to coronavirus_page_path(slug), notice: "Announcements were successfully reordered."
+    else
+      message = "Sorry! Announcements have not been reordered: #{draft_updater.errors.to_sentence}."
+      redirect_to reorder_coronavirus_page_announcements_path(slug), alert: message
+    end
+  end
 
 private
 
@@ -16,5 +29,9 @@ private
 
   def coronavirus_page
     @coronavirus_page ||= CoronavirusPages::ModelBuilder.call(slug)
+  end
+
+  def draft_updater
+    @draft_updater ||= CoronavirusPages::DraftUpdater.new(coronavirus_page)
   end
 end
