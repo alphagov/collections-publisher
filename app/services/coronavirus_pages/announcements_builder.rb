f33125a4369fe30a9489dbf7f3798cbcfc0522c1
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-11-09 12:22:25 +0000

    Only update database if announcements in YAML file
    We want to make sure that we don't accidentally overwrite the existing
    announcements if the YAML doesn't contain any entries i.e. if the task
    is run again after the announcements have been migrated.

--- app/services/coronavirus_pages/announcements_builder.rb
+++ app/services/coronavirus_pages/announcements_builder.rb
@@ -1,14 +1,16 @@
 class CoronavirusPages::AnnouncementsBuilder
   def create_announcements
-    Announcement.transaction do
-      coronavirus_page.announcements.delete_all
+    if announcements_from_yaml.any?
+      Announcement.transaction do
+        coronavirus_page.announcements.delete_all
 
-      announcements_from_yaml.each do |announcement|
-        coronavirus_page.announcements.create!(
-          text: announcement["text"],
-          href: announcement["href"],
-          published_at: Date.parse(announcement["published_text"]),
-        )
+        announcements_from_yaml.each do |announcement|
+          coronavirus_page.announcements.create!(
+            text: announcement["text"],
+            href: announcement["href"],
+            published_at: Date.parse(announcement["published_text"]),
+          )
+        end
       end
     end
   end
@@ -16,8 +18,8 @@ class CoronavirusPages::AnnouncementsBuilder
 private
 
   def announcements_from_yaml
-    github_data = YamlFetcher.new(coronavirus_page.raw_content_url).body_as_hash
-    github_data["content"]["announcements"]
+    @github_data ||= YamlFetcher.new(coronavirus_page.raw_content_url).body_as_hash
+    @github_data["content"]["announcements"]
   end
 
   def coronavirus_page
--- spec/services/coronavirus_pages/announcements_builder_spec.rb
+++ spec/services/coronavirus_pages/announcements_builder_spec.rb
@@ -32,6 +32,25 @@ RSpec.describe CoronavirusPages::AnnouncementsBuilder do
     expect(coronavirus_page.announcements.count).to eq(3)
   end
 
+  it "doesn't remove existing announcements if the YAML file is empty" do
+    github_content = YAML.safe_load(File.read(fixture_path))
+    github_content["content"]["announcements"].clear
+
+    stub_request(:get, Regexp.new(coronavirus_page.raw_content_url))
+      .to_return(status: 200, body: github_content.to_json)
+
+    announcement = create(:announcement, coronavirus_page: coronavirus_page)
+
+    described_class.new.create_announcements
+
+    first_announcement = coronavirus_page.announcements.first
+
+    expect(coronavirus_page.announcements.count).to eq(1)
+    expect(first_announcement.text).to eq(announcement.text)
+    expect(first_announcement.href).to eq(announcement.href)
+    expect(first_announcement.published_at).to eq(announcement.published_at)
+  end
+
   def github_announcement
     github_content["content"]["announcements"].first
   end
