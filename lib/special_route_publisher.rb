2403cef388ac0183ad9f320b208f537c6a0883f5
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-12-09 09:55:47 +0000

    Simplify special route publisher job
    By inverting the merge we can choose what type of route to publish in
    the payload of each route if we wish. We should definitely default to
    `exact` because `prefix` routes should generally be avoided unless there's
    a really good reason for them.
    
    I also don't think there's much need for us to be able to pass custom loggers
    and publishing-api adapters into the publisher. It's just more stuff to
    worry about.

--- lib/special_route_publisher.rb
+++ lib/special_route_publisher.rb
@@ -1,21 +1,22 @@
 require "gds_api/publishing_api/special_route_publisher"
 
 class SpecialRoutePublisher
-  def initialize(publisher_options, publisher = nil)
-    @publishing_api = publisher_options[:publishing_api]
-    @publisher = publisher || GdsApi::PublishingApi::SpecialRoutePublisher.new(publisher_options)
+  def initialize
+    @publishing_api = Services.publishing_api
+    logger = Rails.logger
+    @publisher = GdsApi::PublishingApi::SpecialRoutePublisher.new(logger: logger, publishing_api: @publishing_api)
   end
 
-  def publish(route_type, route)
-    @publisher.publish(
-      route.merge(
-        publishing_app: "collections-publisher",
-        rendering_app: "collections",
-        type: route_type,
-        public_updated_at: Time.zone.now.iso8601,
-        update_type: "major",
-      ),
-    )
+  def publish(route)
+    default_options = {
+      publishing_app: "collections-publisher",
+      rendering_app: "collections",
+      type: "exact",
+      public_updated_at: Time.zone.now.iso8601,
+      update_type: "major",
+    }
+
+    @publisher.publish(default_options.merge(route))
   end
 
   def unpublish(content_id, options)
@@ -23,16 +24,14 @@ class SpecialRoutePublisher
   end
 
   def self.routes
-    {
-      exact: [
-        {
-          document_type: "answer",
-          content_id: "bb986a97-3b8c-4b1a-89bf-2a9f46be9747",
-          base_path: "/eubusiness",
-          title: "Trade with the UK from 1 January 2021 as a business based in the EU",
-          description: "The UK has left the EU. On 31 December 2020 the UK will leave the EU single market and customs union. From 1 January 2021 the rules for trading with the UK will change.",
-        },
-      ],
-    }
+    [
+      {
+        document_type: "answer",
+        content_id: "bb986a97-3b8c-4b1a-89bf-2a9f46be9747",
+        base_path: "/eubusiness",
+        title: "Trade with the UK from 1 January 2021 as a business based in the EU",
+        description: "The UK has left the EU. On 31 December 2020 the UK will leave the EU single market and customs union. From 1 January 2021 the rules for trading with the UK will change.",
+      },
+    ]
   end
 end
