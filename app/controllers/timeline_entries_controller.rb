f5841f0b83f51974d1556400c3107d035e48e13f
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-01-08 12:29:29 +0000

    Create form to enter timeline entry details
    `form_with` is used there as it is replacing `form_tag` and `form_for`
    which have been  deprecated.
    One of the quirks of `form_with` though is that by default `remote` is
    set to true [1][2]. If you don't override this setting with
    `local: true` the form won't submit.
    
    Rather than setting `local: true` in the call to `form_with`, this
    config is being set globally for all forms that use `form_with`. This
    will prevent others from being caught out by the form not submitting,
    and it means we only need to remove one line of config after the
    eventual upgrade to Rails 6.1 where the default is being set back to
    `local` [3].
    
    See:
    [1]: https://edgeguides.rubyonrails.org/form_helpers.html#how-do-forms-with-patch-put-or-delete-methods-work-questionmark
    [2]: https://stackoverflow.com/questions/51892516/rails-form-with-method-get-cannot-update-a-view/51893521
    [3]: https://github.com/rails/rails/pull/40946

--- app/controllers/timeline_entries_controller.rb
+++ app/controllers/timeline_entries_controller.rb
@@ -1,6 +1,15 @@
 class TimelineEntriesController < ApplicationController
   before_action :require_coronavirus_editor_permissions!
   before_action :require_unreleased_feature_permissions!
+  layout "admin_layout"
 
-  def new; end
+  def new
+    @timeline_entry = coronavirus_page.timeline_entries.new
+  end
+
+private
+
+  def coronavirus_page
+    @coronavirus_page ||= CoronavirusPage.find_by(slug: params[:coronavirus_page_slug])
+  end
 end
