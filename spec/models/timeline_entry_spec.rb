e37bdbfa2e0a018b0da491a74e984d58c84156c6
Kevin Dew <kevindew@me.com>
2021-01-08 14:20:43 +0000

    Validate length of timeline entry heading
    Since 255 characters isn't all that long before you get a DB exception
    it seems prudent to have a validation here so the user experience is OK
    if/when someone enters a long heading.
    
    An alternative to this could be to set the column to be a bigger one and
    not validate a limit, however it seems that it'd be rather poor for the
    page visually to use > 255 characters for a heading.

--- spec/models/timeline_entry_spec.rb
+++ spec/models/timeline_entry_spec.rb
@@ -2,5 +2,6 @@ require "rails_helper"
 
 RSpec.describe TimelineEntry do
   it { should validate_presence_of(:heading) }
+  it { should validate_length_of(:heading).is_at_most(255) }
   it { should validate_presence_of(:content) }
 end
