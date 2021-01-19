0748ddf3b240920abc46c643742a0a4dd53d190d
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2021-01-19 16:39:00 +0000

    Add position value when timeline entry is created
    By default, the new timeline entry will be added to the top of the list,
    i.e. position 1, so that they appear in the correct order on the summary
    page and landing page preview, so that content designers can be sure
    that the order of the entries they see in the publishing tool matches
    the order of the timeline entries shown to the user.
    
    For example, a content designer might add an entry for "2 January", and
    later add an entry for "24 - 26 January". By default these entries in
    will be displayed in this order:
    
    "24 -26 January"
    "2 January"
    
    The "position" has been removed from the factory to match what occurs
    when a new timeline entry is created by the user. i.e. it is created
    with a position of `nil`.
    The position is added by the `after_create` callback.
    If we leave the default value in the factory, when `after_create` runs
    the position will be set to 2, even though there's only one timeline
    entry.
    
    This solution isn't the most elegant, but it has the benefit of
    unblocking the rest of the timeline entries work. The other solution
    considered was to use the [acts_as_list][1] gem, but this was taking a
    while to implement. So the decision was made to finish the rest of the
    timeline feature, and then look at improving reordering for both
    announcements and timeline entries.
    
    [1]: https://github.com/brendon/acts_as_list

--- app/models/timeline_entry.rb
+++ app/models/timeline_entry.rb
@@ -2,4 +2,10 @@ class TimelineEntry < ApplicationRecord
   belongs_to :coronavirus_page
   validates :heading, presence: true, length: { maximum: 255 }
   validates :content, presence: true
+  before_create :set_position
+
+  def set_position
+    coronavirus_page.timeline_entries.update_all("position = position + 1")
+    self.position = 1
+  end
 end
--- spec/models/timeline_entry_spec.rb
+++ spec/models/timeline_entry_spec.rb
@@ -4,4 +4,27 @@ RSpec.describe TimelineEntry do
   it { should validate_presence_of(:heading) }
   it { should validate_length_of(:heading).is_at_most(255) }
   it { should validate_presence_of(:content) }
+
+  describe "position" do
+    let(:coronavirus_page) { create(:coronavirus_page) }
+
+    it "should default to position 1 if it is the first timeline entry to have been added" do
+      expect(coronavirus_page.timeline_entries.count).to eq 0
+
+      timeline_entry = create(:timeline_entry, coronavirus_page: coronavirus_page)
+      expect(timeline_entry.reload.position).to eq 1
+    end
+
+    it "if more timeline entries are added, the previous entries will increment position by 1" do
+      expect(coronavirus_page.timeline_entries.count).to eq 0
+
+      first_timeline_entry = create(:timeline_entry, coronavirus_page: coronavirus_page, heading: "one")
+      expect(first_timeline_entry.reload.position).to eq 1
+
+      second_timeline_entry = create(:timeline_entry, coronavirus_page: coronavirus_page, heading: "two")
+
+      expect(first_timeline_entry.reload.position).to eq 2
+      expect(second_timeline_entry.reload.position).to eq 1
+    end
+  end
 end
