a11cd79b0379b68662e0d150ab8d76554149a6b5
Kevin Dew <kevindew@me.com>
2021-02-02 17:33:17 +0000

    Embed TimelineEntryBuilder within a module
    This class (which arrived from a rebase) is the only Coronavirus::Page
    service that is not defined within a module, so I've updated it for
    consistency.

--- app/services/coronavirus/pages/timeline_entry_builder.rb
+++ app/services/coronavirus/pages/timeline_entry_builder.rb
@@ -1,27 +1,29 @@
-class Coronavirus::Pages::TimelineEntryBuilder
-  def create_timeline_entries
-    return if timeline_entries_from_yaml.empty?
+module Coronavirus::Pages
+  class TimelineEntryBuilder
+    def create_timeline_entries
+      return if timeline_entries_from_yaml.empty?
 
-    Coronavirus::TimelineEntry.transaction do
-      coronavirus_page.timeline_entries.delete_all
+      Coronavirus::TimelineEntry.transaction do
+        coronavirus_page.timeline_entries.delete_all
 
-      timeline_entries_from_yaml.reverse.each do |entry|
-        coronavirus_page.timeline_entries.create!(
-          heading: entry["heading"],
-          content: entry["paragraph"],
-        )
+        timeline_entries_from_yaml.reverse.each do |entry|
+          coronavirus_page.timeline_entries.create!(
+            heading: entry["heading"],
+            content: entry["paragraph"],
+          )
+        end
       end
     end
-  end
 
-private
+  private
 
-  def timeline_entries_from_yaml
-    @github_data ||= YamlFetcher.new(coronavirus_page.raw_content_url).body_as_hash
-    @github_data.dig("content", "timeline", "list")
-  end
+    def timeline_entries_from_yaml
+      @github_data ||= YamlFetcher.new(coronavirus_page.raw_content_url).body_as_hash
+      @github_data.dig("content", "timeline", "list")
+    end
 
-  def coronavirus_page
-    @coronavirus_page ||= Coronavirus::CoronavirusPage.find_by(slug: "landing")
+    def coronavirus_page
+      @coronavirus_page ||= Coronavirus::CoronavirusPage.find_by(slug: "landing")
+    end
   end
 end
