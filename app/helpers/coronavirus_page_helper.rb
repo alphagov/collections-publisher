ccb2809afd62e2ac7a4516f65317f15a0f0514a1
Kevin Dew <kevindew@me.com>
2021-02-05 09:44:29 +0000

    Move CoronavirusPageHelper to Coronavirus::PageHelper
    This is consistent with the other coronavirus code being under the
    Coronavirus namespace.

--- app/helpers/coronavirus_page_helper.rb
+++
@@ -1,9 +0,0 @@
-module CoronavirusPageHelper
-  def page_type(page)
-    page.topic_page? ? "Coronavirus landing page" : "Coronavirus hub page"
-  end
-
-  def formatted_title(page)
-    page.topic_page? ? "Coronavirus (COVID-19)" : page.title
-  end
-end
