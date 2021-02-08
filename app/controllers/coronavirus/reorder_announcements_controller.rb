895695f725b0eb192e22b7b9bddca98f7357c086
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-02-08 16:56:34 +0000

    Move reorder announcements controller content
    Move reorder announcements controller content to I18n file.

--- app/controllers/coronavirus/reorder_announcements_controller.rb
+++ app/controllers/coronavirus/reorder_announcements_controller.rb
@@ -24,10 +24,10 @@ module Coronavirus
       end
 
       if success
-        message = "Announcements were successfully reordered."
+        message = I18n.t("coronavirus.pages.announcements.reorder.success")
         redirect_to coronavirus_page_path(page.slug), notice: message
       else
-        message = "Sorry! Announcements have not been reordered: #{draft_updater.errors.to_sentence}."
+        message = I18n.t("coronavirus.pages.announcements.reorder.failed", error: draft_updater.errors.to_sentence)
         redirect_to reorder_coronavirus_page_announcements_path(page.slug), alert: message
       end
     end
