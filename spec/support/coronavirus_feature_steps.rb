754e45657bebd4639742d0bc65d2989fc4f63c18
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-02-22 15:00:03 +0000

    Restructure github changes I18n file view content
    Restructure the content for the github_changes view in the locale file.
    Also replaces hard coded text in the feature tests.

--- spec/support/coronavirus_feature_steps.rb
+++ spec/support/coronavirus_feature_steps.rb
@@ -531,24 +531,24 @@ module CoronavirusFeatureSteps
   end
 
   def i_see_an_update_draft_button
-    expect(page).to have_button("Update draft")
+    expect(page).to have_button(I18n.t("coronavirus.github_changes.index.instructions.three.button_text"))
   end
 
   def and_a_preview_button
-    expect(page).to have_link("Preview")
-    expect(find_link("Preview")[:target]).to eq("_blank")
+    expect(page).to have_link(I18n.t("coronavirus.github_changes.index.instructions.four.button_text"))
+    expect(find_link(I18n.t("coronavirus.github_changes.index.instructions.four.button_text"))[:target]).to eq("_blank")
   end
 
   def and_a_publish_button
-    expect(page).to have_button("Publish")
+    expect(page).to have_button(I18n.t("coronavirus.github_changes.index.instructions.five.button_text"))
   end
 
   def and_a_view_live_business_content_button
-    expect(page).to have_link("View live version", href: "https://www.test.gov.uk/coronavirus/business-support")
+    expect(page).to have_link(I18n.t("coronavirus.github_changes.index.instructions.six.button_text"), href: "https://www.test.gov.uk/coronavirus/business-support")
   end
 
   def and_i_push_a_new_draft_version
-    click_on("Update draft")
+    click_on(I18n.t("coronavirus.github_changes.index.instructions.three.button_text"))
   end
 
   def and_i_push_a_new_draft_version_with_invalid_content
@@ -582,11 +582,11 @@ module CoronavirusFeatureSteps
   end
 
   def and_i_choose_a_major_update
-    choose("Major")
+    choose(I18n.t("coronavirus.github_changes.index.instructions.five.radio_options.major"))
   end
 
   def and_i_publish_the_page
-    click_on("Publish")
+    click_on(I18n.t("coronavirus.github_changes.index.instructions.five.button_text"))
   end
 
   def then_the_page_publishes
