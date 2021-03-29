cb1313cc12a75b2dbd4602d0154290fe3693c7e5
Kevin Dew <kevindew@me.com>
2021-03-29 17:10:49 +0100

    Stop allowing null for position in coronavirus_announcements
    It is assumed that all positions have this field. This field is only not
    null because it is set in an `after_create` callback. If we switch that
    to a `before_create` we can avoid the possibility that this contains
    invalid data.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_143101) do
+ActiveRecord::Schema.define(version: 2021_03_29_150719) do
 
   create_table "coronavirus_announcements", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "coronavirus_page_id", null: false
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_29_143101) do
     t.text "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "position"
+    t.integer "position", null: false
     t.date "published_on"
   end
 
