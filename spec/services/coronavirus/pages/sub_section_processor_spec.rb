e12bbeabf4263ebef27358bf5ebce410fab1f432
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-05-05 12:05:09 +0100

    Give test data a more descriptive name
    Also moves the variable assignment to inside the describe block to be
    closer to the tests using it.

--- spec/services/coronavirus/pages/sub_section_processor_spec.rb
+++ spec/services/coronavirus/pages/sub_section_processor_spec.rb
@@ -7,44 +7,45 @@ RSpec.describe Coronavirus::Pages::SubSectionProcessor do
   let(:url) { "/#{File.join(Faker::Lorem.words)}" }
   let(:label_1) { Faker::Lorem.sentence }
   let(:url_1) { "/#{File.join(Faker::Lorem.words)}?priority-taxon=774cee22-d896-44c1-a611-e3109cce8eae" }
-  let(:data) do
-    [
-      {
-        "title" => title,
-        "list" => [
-          {
-            "label" => label,
-            "url" => url,
-            "featured_link" => true,
-            "description" => description,
-          },
-          {
-            "label" => label_1,
-            "url" => url_1,
-          },
-        ],
-      },
-    ]
-  end
 
   describe ".call" do
+    let(:sub_section_payload_data) do
+      [
+        {
+          "title" => title,
+          "list" => [
+            {
+              "label" => label,
+              "url" => url,
+              "featured_link" => true,
+              "description" => description,
+            },
+            {
+              "label" => label_1,
+              "url" => url_1,
+            },
+          ],
+        },
+      ]
+    end
+
     it "creates the correct number of lines" do
       # 2 = 1 title plus 1 link - feature_link's aren't created
-      output = described_class.call(data)
+      output = described_class.call(sub_section_payload_data)
       lines = output[:content].split("\n")
 
       expect(lines.count).to eq(2)
     end
 
     it "has title as the first line" do
-      output = described_class.call(data)
+      output = described_class.call(sub_section_payload_data)
       lines = output[:content].split("\n")
 
       expect(lines.first).to eq "####{title}"
     end
 
     it "stores the url, label and description in the appropriate action_link fields" do
-      output = described_class.call(data)
+      output = described_class.call(sub_section_payload_data)
 
       expect(output).to match hash_including(
         action_link_url: url,
@@ -54,14 +55,14 @@ RSpec.describe Coronavirus::Pages::SubSectionProcessor do
     end
 
     it "removes any priority-taxons query parameters from any non-featured links" do
-      output = described_class.call(data)
+      output = described_class.call(sub_section_payload_data)
       lines = output[:content].split("\n")
 
       expect(lines.second).to eq "[#{label_1}](#{url_1.gsub('?priority-taxon=774cee22-d896-44c1-a611-e3109cce8eae', '')})"
     end
 
     context "with blank title" do
-      let(:data) do
+      let(:sub_section_payload_data) do
         {
           "title" => nil,
           "list" => [
@@ -74,7 +75,7 @@ RSpec.describe Coronavirus::Pages::SubSectionProcessor do
       end
 
       it "has the first link as its first line" do
-        output = described_class.call(data)
+        output = described_class.call(sub_section_payload_data)
         lines = output[:content].split("\n")
 
         expect(lines.first).to eq "[#{label}](#{url})"
