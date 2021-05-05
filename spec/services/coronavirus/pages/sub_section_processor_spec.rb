ca6c9fcce0a84285cd6c6c7d44d39891fc83f989
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-05-05 12:03:21 +0100

    Move the method call to within the tests
    This makes it easier to see exactly what method is being tested and what
    params it's being passed without having to scroll up the page.

--- spec/services/coronavirus/pages/sub_section_processor_spec.rb
+++ spec/services/coronavirus/pages/sub_section_processor_spec.rb
@@ -28,20 +28,25 @@ RSpec.describe Coronavirus::Pages::SubSectionProcessor do
   end
 
   describe ".call" do
-    subject { described_class.call(data) }
-    let(:lines) { subject[:content].split("\n") }
-
     it "creates the correct number of lines" do
       # 2 = 1 title plus 1 link - feature_link's aren't created
+      output = described_class.call(data)
+      lines = output[:content].split("\n")
+
       expect(lines.count).to eq(2)
     end
 
     it "has title as the first line" do
+      output = described_class.call(data)
+      lines = output[:content].split("\n")
+
       expect(lines.first).to eq "####{title}"
     end
 
     it "stores the url, label and description in the appropriate action_link fields" do
-      expect(subject).to match hash_including(
+      output = described_class.call(data)
+
+      expect(output).to match hash_including(
         action_link_url: url,
         action_link_content: label,
         action_link_summary: description,
@@ -49,6 +54,9 @@ RSpec.describe Coronavirus::Pages::SubSectionProcessor do
     end
 
     it "removes any priority-taxons query parameters from any non-featured links" do
+      output = described_class.call(data)
+      lines = output[:content].split("\n")
+
       expect(lines.second).to eq "[#{label_1}](#{url_1.gsub('?priority-taxon=774cee22-d896-44c1-a611-e3109cce8eae', '')})"
     end
 
@@ -66,6 +74,9 @@ RSpec.describe Coronavirus::Pages::SubSectionProcessor do
       end
 
       it "has the first link as its first line" do
+        output = described_class.call(data)
+        lines = output[:content].split("\n")
+
         expect(lines.first).to eq "[#{label}](#{url})"
       end
     end
