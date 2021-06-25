21ca39fb75bed9174c0c6227dcc0fa6812ccc552
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2021-06-25 16:25:59 +0100

    Update rails version to 6.1.4

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 
 module CollectionsPublisher
   class Application < Rails::Application
-    config.load_defaults "6.0"
+    config.load_defaults "6.1"
     config.time_zone = "London"
     config.action_view.raise_on_missing_translations = true
     config.active_record.belongs_to_required_by_default = false
