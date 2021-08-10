e06854b8ec40cd47fd01ece58a9cac2d1bafb980
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2021-08-10 18:39:12 +0100

    Display YAML editor on view
    - Basic configuration for adding an ace-builds YAML editor

--- app/assets/javascripts/admin_layout.js
+++ app/assets/javascripts/admin_layout.js
@@ -4,6 +4,9 @@
 //= require jquery-ui.sortable.min
 //= require jquery_ujs
 
+//= require ace-builds/src/ace
+//= require ace-builds/src/mode-yaml
+
 // support ES5
 //= require es5-polyfill/dist/polyfill.js
 
@@ -14,3 +17,8 @@
 //= require @webcomponents/custom-elements/custom-elements.min.js
 
 //= require components/markdown-editor.js
+
+var editor = ace.edit("editor");
+
+var YamlMode = ace.require("ace/mode/yaml").Mode;
+editor.session.setMode(new YamlMode());
