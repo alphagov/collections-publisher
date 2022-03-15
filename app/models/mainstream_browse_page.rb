47c83d4f2780ab1d3ff41440e868efef4c640c9f
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2022-03-15 11:05:14 +0000

    Convert Manage child ordering page
    This page needs additonal work to unpick the weird hidden field issue. We should use the reorderable component instead as it's a better use experience.

--- app/models/mainstream_browse_page.rb
+++ app/models/mainstream_browse_page.rb
@@ -2,6 +2,7 @@ class MainstreamBrowsePage < Tag
   has_many :topics, through: :tag_associations, source: :to_tag
 
   validate :parents_cannot_have_topics_associated
+  validate :children_index_present, on: :update
 
   accepts_nested_attributes_for :children
 
@@ -30,6 +31,17 @@ private
   def parents_cannot_have_topics_associated
     if parent.blank? && topics.any?
       errors.add(:topics, "top-level mainstream browse pages cannot have topics assigned to them")
+    end
+  end
+
+  def children_index_present
+    return if children.blank?
+
+    sorted_children = sorted_children_that_are_not_archived
+    children_with_no_index = sorted_children.select { |child| child.index.nil? }
+
+    children_with_no_index.each do |child|
+      errors.add("children_attributes_#{sorted_children.find_index(child)}_index".to_sym, message: "Enter an index for #{child.title}")
     end
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -17,9 +17,9 @@ class Tag < ApplicationRecord
   has_many :redirect_routes
 
   validates :slug, presence: { message: "Enter a slug" }
+  validates :slug, uniqueness: { scope: %w[parent_id], case_sensitive: false }, format: { with: /\A[a-z0-9-]*\z/, message: "Enter a valid slug" }
   validates :title, presence: { message: "Enter a title" }
   validates :content_id, presence: true
-  validates :slug, uniqueness: { scope: %w[parent_id], case_sensitive: false }, format: { with: /\A[a-z0-9-]*\z/, message: "Enter a valid slug" }
   validates :child_ordering, inclusion: { in: ORDERING_TYPES }
   validate :parent_is_not_a_child
   validate :cannot_change_slug
