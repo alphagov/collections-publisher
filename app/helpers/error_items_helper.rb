a626e36bec5cf4c7f2afbe1b4db8dc4baedaf366
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-03-24 14:33:36 +0000

    Move error helper method into ErrorItemHelper

--- app/helpers/error_items_helper.rb
+++ app/helpers/error_items_helper.rb
@@ -4,4 +4,14 @@ module ErrorItemsHelper
       error_messages[field].map { |message| "#{field.to_s.titleize} #{message}".capitalize }.join("\n")
     end
   end
+
+  def issues_for(object, attribute)
+    object.errors.errors.filter_map do |error|
+      if error.attribute == attribute
+        {
+          text: error.options[:message],
+        }
+      end
+    end
+  end
 end
