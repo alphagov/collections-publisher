d4233facc99ea8176d8ff391f7b4dff1e2ea6253
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2022-05-06 15:21:42 +0100

    Add a rake task to copy one Mainstream browse tree into Topics
    We initially want to test it with only one topic tree

--- lib/tasks/browse_topics.rake
+++ lib/tasks/browse_topics.rake
@@ -10,4 +10,22 @@ namespace :browse_topics do
       MainstreamBrowsePage.where(state: :published),
     )
   end
+
+  desc "Copy one published Mainstream browse pages tree to Topics"
+  task :copy_mainstream_browse_pages_tree_to_topics, [:mainstream_browse_page_content_id] => :environment do |_task, args|
+    raise "This rake task is not intended to be run in production" if Rails.env.production?
+
+    mainstream_browse_page = MainstreamBrowsePage.find_by(content_id: args.mainstream_browse_page_content_id)
+
+    raise "You can only copy over published Mainstream browse pages" unless mainstream_browse_page && mainstream_browse_page.published?
+
+    top_level_mainstream_browse_page = mainstream_browse_page.parent.nil? ? mainstream_browse_page : mainstream_browse_page.parent
+
+    mainstream_browse_pages_in_the_tree = [
+      top_level_mainstream_browse_page,
+      top_level_mainstream_browse_page.children,
+    ].flatten.compact
+
+    CopyMainstreamBrowsePagesToTopics.call(mainstream_browse_pages_in_the_tree)
+  end
 end
--- spec/lib/tasks/browse_topics_spec.rb
+++ spec/lib/tasks/browse_topics_spec.rb
@@ -30,3 +30,58 @@ RSpec.describe "rake browse_topics:copy_mainstream_browse_pages_to_topics", type
     expect(CopyMainstreamBrowsePagesToTopics).not_to have_received(:call)
   end
 end
+
+RSpec.describe "rake browse_topics:copy_mainstream_browse_pages_tree_to_topics", type: :task do
+  before do
+    allow(CopyMainstreamBrowsePagesToTopics).to receive(:call)
+    Rake::Task["browse_topics:copy_mainstream_browse_pages_tree_to_topics"].reenable
+  end
+
+  it "doesn't run in production" do
+    allow(Rails).to receive(:env) { "production".inquiry }
+
+    expect { Rake::Task["browse_topics:copy_mainstream_browse_pages_tree_to_topics"].invoke }.to raise_error(
+      "This rake task is not intended to be run in production",
+    )
+
+    expect(CopyMainstreamBrowsePagesToTopics).not_to have_received(:call)
+  end
+
+  it "doesn't try to copy over not published mainstream browse pages" do
+    create(:mainstream_browse_page)
+    draft = create(:mainstream_browse_page, :draft)
+
+    expect { Rake::Task["browse_topics:copy_mainstream_browse_pages_tree_to_topics"].invoke(draft.content_id) }.to raise_error(
+      "You can only copy over published Mainstream browse pages",
+    )
+
+    expect(CopyMainstreamBrowsePagesToTopics).not_to have_received(:call)
+  end
+
+  it "calls the service with all pages in the tree, given a top level topic" do
+    create(:mainstream_browse_page)
+    create(:mainstream_browse_page, :archived)
+    top_level = create(:mainstream_browse_page, :published)
+    second_level_child = create(:mainstream_browse_page, :published, parent: top_level)
+    second_level_sibling = create(:mainstream_browse_page, :published, parent: top_level)
+
+    Rake::Task["browse_topics:copy_mainstream_browse_pages_tree_to_topics"].invoke(top_level.content_id)
+
+    expect(CopyMainstreamBrowsePagesToTopics).to have_received(:call).exactly(1).time
+    expect(CopyMainstreamBrowsePagesToTopics).to have_received(:call).with(
+      contain_exactly(top_level, second_level_child, second_level_sibling),
+    )
+  end
+
+  it "calls the service with all pages in the tree given a second level topic" do
+    top_level = create(:mainstream_browse_page, :published)
+    second_level_child = create(:mainstream_browse_page, :published, parent: top_level)
+    second_level_sibling = create(:mainstream_browse_page, :published, parent: top_level)
+
+    Rake::Task["browse_topics:copy_mainstream_browse_pages_tree_to_topics"].invoke(second_level_child.content_id)
+
+    expect(CopyMainstreamBrowsePagesToTopics).to have_received(:call).with(
+      contain_exactly(top_level, second_level_child, second_level_sibling),
+    )
+  end
+end
