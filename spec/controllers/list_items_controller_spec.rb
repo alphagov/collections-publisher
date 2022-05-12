69249d563dd44d973a315a6b43fc2f5dd517ff78
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-05-12 13:29:50 +0100

    Add endpoint for moving a list item to another list
    This adds in the route, view and controller actions for moving a list
    item to another list.
    
    It does a few things:
    1. checks that an id is passed through from the view. If not it renders
    an error.
    2. moves the list item to the new list
    4. pushes the changes downstream to the PublishingAPI

--- spec/controllers/list_items_controller_spec.rb
+++ spec/controllers/list_items_controller_spec.rb
@@ -360,4 +360,191 @@ RSpec.describe ListItemsController, type: :controller do
       end
     end
   end
+
+  describe "GET move" do
+    let(:list) { create(:list, tag: tag) }
+    let!(:list_item) { create(:list_item, list: list) }
+
+    context "Tag is a MainstreamBrowsePage" do
+      let(:tag) { create(:mainstream_browse_page) }
+
+      it "assign the correct instance variables and renders the move template" do
+        stub_user.update!(permissions: ["signin", "GDS Editor", "Redesigned lists"])
+
+        get :move, params: { tag_id: tag.content_id, list_id: list.id, id: list_item.id }
+
+        expect(assigns(:tag).id).to eq tag.id
+        expect(assigns(:list).id).to eq list.id
+        expect(assigns(:list_item).id).to eq list_item.id
+        expect(response.status).to eq(200)
+        expect(response).to render_template :move
+      end
+
+      it "does not allow users without GDS Editor permissions access" do
+        stub_user.update!(permissions: ["signin", "Redesigned lists"])
+
+        get :move, params: { tag_id: tag.content_id, list_id: list.id, id: list_item.id }
+
+        expect(response.status).to eq(403)
+      end
+    end
+
+    context "Tag is a topic and user does not have `GDS Editor permissions`" do
+      let(:tag) { create(:topic) }
+
+      it "assign the correct instance vaiables and renders the move template" do
+        stub_user.update!(permissions: ["signin", "Redesigned lists"])
+
+        get :move, params: { tag_id: tag.content_id, list_id: list.id, id: list_item.id }
+
+        expect(assigns(:tag).id).to eq tag.id
+        expect(assigns(:list).id).to eq list.id
+        expect(assigns(:list_item).id).to eq list_item.id
+        expect(response.status).to eq(200)
+        expect(response).to render_template :move
+      end
+    end
+  end
+
+  describe "PATCH update_move" do
+    let(:list1) { create(:list, tag: tag) }
+    let(:list2) { create(:list, tag: tag) }
+    let!(:list_item1) { create(:list_item, list: list1, index: 1) }
+    let!(:list_item2) { create(:list_item, list: list1, index: 2) }
+    let(:tagged_documents) do
+      [
+        TaggedDocuments::Document.new(list_item1.title, list_item1.base_path, "123"),
+        TaggedDocuments::Document.new(list_item2.title, list_item2.base_path, "456"),
+        TaggedDocuments::Document.new("New list", "/new-list", "789"),
+      ]
+    end
+
+    before do
+      stub_any_publishing_api_call
+      allow_any_instance_of(TaggedDocuments).to receive(:documents).and_return(tagged_documents)
+    end
+
+    context "Tag is a MainstreamBrowsePage" do
+      let(:tag) { create(:mainstream_browse_page, :published) }
+
+      it "updates the list item to belong to list_id passed in and makes the correct calls to the Publishing API" do
+        stub_user.update!(permissions: ["signin", "GDS Editor", "Redesigned lists"])
+
+        patch :update_move, params: {
+          tag_id: tag.content_id,
+          list_id: list1.id,
+          id: list_item1.id,
+          list_item: { new_list_id: list2.id },
+        }
+
+        expect(list1.reload.list_items.count).to eq 1
+        expect(list2.reload.list_items.count).to eq 1
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(tag_list_path(tag, list1))
+        expect(flash.notice).to eq "Item moved to new list successfully"
+        assert_publishing_api_put_content(
+          tag.content_id,
+          request_json_includes(
+            "details" => {
+              "groups" => [
+                {
+                  "name" => list1.name,
+                  "contents" => [
+                    list_item2.base_path,
+                  ],
+                },
+                {
+                  "name" => list2.name,
+                  "contents" => [
+                    list_item1.base_path,
+                  ],
+                },
+              ],
+              "internal_name" => tag.title,
+              "second_level_ordering" => "alphabetical",
+              "ordered_second_level_browse_pages" => [],
+            },
+          ),
+        )
+        assert_publishing_api_publish(tag.content_id)
+        assert_publishing_api_patch_links(tag.content_id)
+      end
+
+      it "does not allow users without GDS Editor permissions access" do
+        stub_user.update!(permissions: ["signin", "Redesigned lists"])
+
+        patch :update_move, params: {
+          tag_id: tag.content_id,
+          list_id: list1.id,
+          id: list_item1.id,
+          list_item: { new_list_id: list2.id },
+        }
+
+        expect(response.status).to eq(403)
+      end
+
+      it "adds and error and renders the move view if a new_list_id is not passed in" do
+        stub_user.update!(permissions: ["signin", "GDS Editor", "Redesigned lists"])
+
+        patch :update_move, params: {
+          tag_id: tag.content_id,
+          list_id: list1.id,
+          id: list_item1.id,
+        }
+
+        expect(assigns(:tag).id).to eq tag.id
+        expect(assigns(:list).id).to eq list1.id
+        expect(assigns(:list_item).id).to eq list_item1.id
+        expect(assigns(:list_item).errors.first.message).to eq "Choose a list"
+        expect(response.status).to eq(200)
+        expect(response).to render_template :move
+      end
+    end
+
+    context "Tag is a topic and user does not have `GDS Editor permissions`" do
+      let(:tag) { create(:topic, :published) }
+
+      it "updates the list item to belong to list_id passed in and makes the correct calls to the Publishing API" do
+        stub_user.update!(permissions: ["signin", "Redesigned lists"])
+
+        patch :update_move, params: {
+          tag_id: tag.content_id,
+          list_id: list1.id,
+          id: list_item2.id,
+          list_item: { new_list_id: list2.id },
+        }
+
+        expect(list1.reload.list_items.count).to eq 1
+        expect(list2.reload.list_items.count).to eq 1
+        expect(list2.list_items.first.index).to eq 1
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(tag_list_path(tag, list1))
+        expect(flash.notice).to eq "Item moved to new list successfully"
+        assert_publishing_api_put_content(
+          tag.content_id,
+          request_json_includes(
+            "details" => {
+              "groups" => [
+                {
+                  "name" => list1.name,
+                  "contents" => [
+                    list_item1.base_path,
+                  ],
+                },
+                {
+                  "name" => list2.name,
+                  "contents" => [
+                    list_item2.base_path,
+                  ],
+                },
+              ],
+              "internal_name" => tag.title,
+            },
+          ),
+        )
+        assert_publishing_api_publish(tag.content_id)
+        assert_publishing_api_patch_links(tag.content_id)
+      end
+    end
+  end
 end
