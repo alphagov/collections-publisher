fe506dd67ed934c914d104fbed6f0afa014b0fae
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-05-17 13:30:09 +0100

    Run rails app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_150444) do
+ActiveRecord::Schema[6.1].define(version: 2022_04_13_150444) do
 
   create_table "coronavirus_pages", charset: "utf8mb3", force: :cascade do |t|
     t.string "sections_title"
