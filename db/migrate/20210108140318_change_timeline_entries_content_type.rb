77d1338b26492b99bbf7c333f281267a766d303c
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-05-18 09:15:49 +0100

    Appease rubocop by making migration reversible

--- db/migrate/20210108140318_change_timeline_entries_content_type.rb
+++ db/migrate/20210108140318_change_timeline_entries_content_type.rb
@@ -1,5 +1,9 @@
 class ChangeTimelineEntriesContentType < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :timeline_entries, :content, :text
+  end
+
+  def down
+    change_column :timeline_entries, :content, :string
   end
 end
