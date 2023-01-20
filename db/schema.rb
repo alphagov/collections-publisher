897898a474dc49b8bb052f2df87b4a422c22e452
Barbara Slawinska <barbara.slawinska@digital.cabinet-office.gov.uk>
2023-01-20 14:41:17 +0000

    Manually remove topics for MainstreamBrowsePage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_09_124352) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_143600) do
   create_table "coronavirus_pages", charset: "utf8mb3", force: :cascade do |t|
     t.string "sections_title"
     t.string "base_path"
