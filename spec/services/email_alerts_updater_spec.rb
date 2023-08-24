3e1597e4c304161b05c2e2f40033ea39a8476ef0
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2023-08-24 12:03:09 +0100

    Refactor tests
    So that the context blocks are grouped by when we bulk migrate,
    and when we bulk unsubscribe

--- spec/services/email_alerts_updater_spec.rb
+++ spec/services/email_alerts_updater_spec.rb
@@ -5,21 +5,22 @@ RSpec.describe EmailAlertsUpdater do
   include GdsApi::TestHelpers::EmailAlertApi
 
   let(:topic) { create(:topic, title: "Child benefit", slug: "child-benefit") }
+
+  let(:successor) { instance_double(ContentItem) }
+  let(:successor_base_path) { "/successor-base-path" }
+
   let(:email_alert_api_endpoint) { GdsApi::TestHelpers::EmailAlertApi::EMAIL_ALERT_API_ENDPOINT }
+  let(:bulk_unsubscribe_link) { "#{email_alert_api_endpoint}/subscriber-lists/tax-credits-and-child-benefit-child-benefit/bulk-unsubscribe" }
+  let(:bulk_migrate_link) { "#{email_alert_api_endpoint}/subscriber-lists/bulk-migrate" }
+  let(:expected_unsubscribe_email_message) do
+    <<~BODY
+      This topic has been archived. You will not get any more emails about it.
+
+      You can find more information about this topic at [#{Plek.website_root}#{successor_base_path}](#{Plek.website_root}#{successor_base_path}).
+    BODY
+  end
 
   describe ".call" do
-    let(:successor) { instance_double(ContentItem) }
-    let(:successor_base_path) { "/successor-base-path" }
-    let(:expected_unsubscribe_email_message) do
-      <<~BODY
-        This topic has been archived. You will not get any more emails about it.
-
-        You can find more information about this topic at [#{Plek.website_root}#{successor_base_path}](#{Plek.website_root}#{successor_base_path}).
-      BODY
-    end
-    let(:bulk_unsubscribe_link) { "#{email_alert_api_endpoint}/subscriber-lists/tax-credits-and-child-benefit-child-benefit/bulk-unsubscribe" }
-    let(:bulk_migrate_link) { "#{email_alert_api_endpoint}/subscriber-lists/bulk-migrate" }
-
     before do
       allow(successor).to receive(:base_path).and_return(successor_base_path)
       allow(SecureRandom).to receive(:uuid).and_return("some-uuid")
@@ -32,66 +33,52 @@ RSpec.describe EmailAlertsUpdater do
       )
     end
 
-    context "when the successor has a mapped specialist topic content id that does not match the item's content id" do
-      before do
-        allow(successor).to receive(:mapped_specialist_topic_content_id).and_return("e3c4f270-044b-11ee-be56-0242ac120002")
-      end
+    context "bulk unsubscribing specialist topic email subscribers" do
 
-      it "makes a call the email-alert-api bulk unsubscribe endpoint with the slug, and generated email body and sender_message_id" do
-        bulk_unsubscribe_stub = stub_request(:post, bulk_unsubscribe_link)
-                                  .with(body: { body: expected_unsubscribe_email_message, sender_message_id: "some-uuid" })
-                                  .to_return(status: 200)
-
-        expect(Services.email_alert_api).to receive(:bulk_migrate).never
-
-        described_class.call(item: topic, successor:)
-        expect(bulk_unsubscribe_stub).to have_been_requested
-      end
+    let!(:bulk_unsubscribe_stub) do
+      stub_request(:post, bulk_unsubscribe_link)
+        .with(body: { body: expected_unsubscribe_email_message, sender_message_id: "some-uuid" })
+        .to_return(status: 200)
     end
 
-    context "when the success has no mapped specialist topic content id" do
-      before do
+      it "mapped_specialist_topic_content_id is nil" do
         allow(successor).to receive(:mapped_specialist_topic_content_id).and_return(nil)
-      end
-
-      it "makes a call the email-alert-api bulk unsubscribe endpoint with the slug, and generated email body and sender_message_id" do
-        bulk_unsubscribe_stub = stub_request(:post, bulk_unsubscribe_link)
-                                  .with(body: { body: expected_unsubscribe_email_message, sender_message_id: "some-uuid" })
-                                  .to_return(status: 200)
-
-        expect(Services.email_alert_api).to receive(:bulk_migrate).never
 
         described_class.call(item: topic, successor:)
         expect(bulk_unsubscribe_stub).to have_been_requested
+        expect(Services.email_alert_api).to receive(:bulk_migrate).never
+      end
+
+      it "mapped_specialist_topic_content_id is present but does not match specialist topic content id" do
+        allow(successor).to receive(:mapped_specialist_topic_content_id).and_return("i-do-not-match")
+
+        described_class.call(item: topic, successor:)
+        expect(bulk_unsubscribe_stub).to have_been_requested
+        expect(Services.email_alert_api).to receive(:bulk_migrate).never
       end
     end
 
-    context "when the successor has the same mapped specialist topic id as the topic it is replacing" do
-      let(:successor_data) do
-        {
-          "title" => "Successor document collection",
-          "base_path" => successor_base_path,
-          "content_id" => "835af2ae-ed12-49f0-9b3c-d6795d028484",
-          "document_type" => "document_collection",
-          "description" => "description for Successor document collection",
+    context "bulk migrating specialist topic email subscribers" do
+      let(:successor_content_id){"835af2ae-ed12-49f0-9b3c-d6795d028484"}
+
+      let(:successor) do
+        ContentItem.new(
+          "content_id" => successor_content_id,
           "details" => {
             "mapped_specialist_topic_content_id" => topic.content_id,
           },
-        }
+        )
       end
-      let(:successor) { ContentItem.new(successor_data) }
 
       before do
         stub_email_alert_api_creates_subscriber_list(
-          "title" => "Successor document collection",
           "links" => { "document_collections" => [SecureRandom.uuid] },
-          "content_id" => "835af2ae-ed12-49f0-9b3c-d6795d028484",
-          "description" => "description for Successor document collection",
+          "content_id" => successor_content_id,
           "slug" => "slug",
         )
       end
 
-      it "makes a call the email-alert-api bulk migrate endpoint with the slug of the subscriber list of the document collection" do
+      it "successor has a mapped specialist topic content id that matches the specialist topic's content id" do
         bulk_migrate_stub = stub_request(:post, bulk_migrate_link)
                               .with(body: { to_slug: "slug", from_slug: "tax-credits-and-child-benefit-child-benefit" })
                               .to_return(status: 200)
