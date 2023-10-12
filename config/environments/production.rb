10e7e60b5dbd07a9ec4678bf0fe38fd9996d0227
Chris Whitehouse <chris.whitehouse@digital.cabinet-office.gov.uk>
2023-10-12 13:17:17 +0100

    Update js_compressor config to use updated Uglifier syntax to be compatible with rails 7.1.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Compress JavaScript.
-  config.assets.js_compressor = :uglifier
-
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
