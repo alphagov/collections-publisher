de4007d3c11fbb05b76bc7df54cfa86a0f18f58e
Barbara Slawinska <barbara.slawinska@digital.cabinet-office.gov.uk>
2024-06-20 16:50:38 +0100

    Changes to test new gem version

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,31 @@
 // system aspects of Collections Publisher
 
 //= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/all_components
+//= require govuk_publishing_components/components/back-link
+//= require govuk_publishing_components/components/breadcrumbs
+//= require govuk_publishing_components/components/checkboxes
+//= require govuk_publishing_components/components/contextual-guidance
+//= require govuk_publishing_components/components/date-input
+//= require govuk_publishing_components/components/details
+//= require govuk_publishing_components/components/document-list
+//= require govuk_publishing_components/components/error-alert
+//= require govuk_publishing_components/components/error-message
+//= require govuk_publishing_components/components/govspeak
+//= require govuk_publishing_components/components/heading
+//= require govuk_publishing_components/components/hint
+//= require govuk_publishing_components/components/input
+//= require govuk_publishing_components/components/inset-text
+//= require govuk_publishing_components/components/label
+//= require govuk_publishing_components/components/layout-footer
+//= require govuk_publishing_components/components/layout-for-admin
+//= require govuk_publishing_components/components/list
+//= require govuk_publishing_components/components/metadata
+//= require govuk_publishing_components/components/reorderable-list
+//= require govuk_publishing_components/components/select
+//= require govuk_publishing_components/components/success-alert
+//= require govuk_publishing_components/components/summary-list
+//= require govuk_publishing_components/components/table
+//= require govuk_publishing_components/components/textarea
 //= require govuk_publishing_components/analytics
 //= require components/autocomplete
 //= require analytics
