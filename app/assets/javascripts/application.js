12b4ddf1141a3d1ec4c5e875f94dd67e065088b4
davidtrussler <mail@davidtrussler.net>
2024-06-28 12:04:34 +0100

    Update JS imports

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,32 +2,15 @@
 // system aspects of Collections Publisher
 
 //= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/components/back-link
-//= require govuk_publishing_components/components/breadcrumbs
+//= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/checkboxes
 //= require govuk_publishing_components/components/contextual-guidance
-//= require govuk_publishing_components/components/date-input
 //= require govuk_publishing_components/components/details
-//= require govuk_publishing_components/components/document-list
-//= require govuk_publishing_components/components/error-alert
-//= require govuk_publishing_components/components/error-message
 //= require govuk_publishing_components/components/govspeak
-//= require govuk_publishing_components/components/heading
-//= require govuk_publishing_components/components/hint
-//= require govuk_publishing_components/components/input
-//= require govuk_publishing_components/components/inset-text
-//= require govuk_publishing_components/components/label
-//= require govuk_publishing_components/components/layout-footer
-//= require govuk_publishing_components/components/layout-for-admin
-//= require govuk_publishing_components/components/list
 //= require govuk_publishing_components/components/metadata
 //= require govuk_publishing_components/components/reorderable-list
-//= require govuk_publishing_components/components/select
-//= require govuk_publishing_components/components/success-alert
-//= require govuk_publishing_components/components/summary-list
 //= require govuk_publishing_components/components/table
-//= require govuk_publishing_components/components/textarea
-//= require govuk_publishing_components/analytics
+//= require govuk_publishing_components/components/tabs
 //= require components/autocomplete
 //= require analytics
 
