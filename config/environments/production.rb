05760af3bcdb219444051eb4688bf5e6bea85b75
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-10-08 10:18:36 +0100

    Configure hosts for application
    Note: the healthcheck endpoints are requested by IP, not domain, so we
    need to specifically exclude them from the protection.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.unreleased_features = ENV["UNRELEASED_FEATURES"].present?
+
+  # Enable DNS rebinding protection and other `Host` header attacks.
+  config.hosts = [
+    /collections-publisher\..*gov.uk?/,
+  ]
+
+  # Skip DNS rebinding protection for the default health check endpoint.
+  config.host_authorization = { exclude: ->(request) { request.path.match?("^\/healthcheck") } }
 end
