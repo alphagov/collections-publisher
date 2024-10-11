77c4ce489110b361c27dacbc196f4aef08e1257e
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-10-11 15:08:05 +0100

    Update host matching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    /collections-publisher\..*gov.uk?/,
+    /collections-publisher\..*\.gov.uk/,
   ]
 
   # Skip DNS rebinding protection for the default health check endpoint.
