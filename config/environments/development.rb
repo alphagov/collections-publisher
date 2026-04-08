75b8519c77f4bbadb9f9a8afa6bdc71cb6f4bbd2
Alex Sheppard <132467929+AlexRyanShep@users.noreply.github.com>
2026-04-08 12:13:21 +0100

    Update Rails from 8.0.3 to 8.1.3
    Accept some new environment config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -61,6 +62,15 @@ Rails.application.configure do
 
   # To see the latest stylesheet changes, if running Sass in watch mode.
   config.assets.digest = false
+
+  # Append comments with runtime information tags to SQL queries in logs.
+  config.active_record.query_log_tags_enabled = true
+
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
+
+  # Highlight code that triggered redirect in logs.
+  config.action_dispatch.verbose_redirect_logs = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
