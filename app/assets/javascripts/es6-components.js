eff93f2cc4bf1417d7bb639f474d5b3cb9193d39
Rob Young <robert.young@digital.cabinet-office.gov.uk>
2026-08-24 12:32:58 +0100

    Remove govuk_publishing_components/components/layout-header es6 import
    This has been removed as of govuk_publishing_components v68.0

--- app/assets/javascripts/es6-components.js
+++ app/assets/javascripts/es6-components.js
@@ -11,6 +11,5 @@
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/character-count
 //= require govuk_publishing_components/components/error-summary
-//= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/skip-link
